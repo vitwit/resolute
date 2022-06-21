65ed2b37b7a41751f0edcc4e300f29ac34d3d97e
aleem1314 <aleem@vitwit.com>
2022-06-21 15:11:49 +0530

    chore: fix date parsing

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,5 +1,5 @@
+import moment from 'moment/moment';
+
 export function getLocalTime(value) {
-    const datePattern = /^(\d{4})-(\d{2})-(\d{2})\s(\d{1,2}):(\d{2})$/;
-    const [, year, month, day, rawHour, min] = datePattern.exec('2010-10-20 4:30');
-    return new Date(`${year}-${month}-${day}T${('0' + rawHour).slice(-2)}:${min}:00`);
+    return moment(value).format("YYYY-MM-DD HH:m:s")
 }
