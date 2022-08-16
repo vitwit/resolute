6974df135c5b6fb1c87fc72a5629c7489bc92ac8
MD Aleem <72057206+aleem1314@users.noreply.github.com>
2022-08-16 11:14:27 +0530

    group: add create group (#140)
    wip

--- src/features/group/groupSlice.js
+++ src/features/group/groupSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { CreateGroup, CreateGroupWithPolicy } from "../../txns/proto";
 import groupService from "./groupService";
 
 const initialState = {
@@ -44,6 +45,59 @@ export const getGroupsByMember = createAsyncThunk(
   }
 );
 
+export const txCreateGroup = createAsyncThunk(
+  "group/tx-create-group",
+  async (data, { rejectWithValue, fulfillWithValue, dispatch }) => {
+    let msg;
+    try {
+      if (data.members.length > 0) {
+        if (data.decisionPolicy) {
+          msg = CreateGroupWithPolicy(
+            data.admin,
+            data.groupMetadata,
+            data.members,
+            data.decisionPolicy,
+            data.policyMetadata,
+            data.policyAsAdmin
+          );
+        }
+        msg = CreateGroup(data.admin, data.groupMetadata, data.members);
+      } else {
+        msg = CreateGroup(data.admin, data.groupMetadata, []);
+      }
+      const result = await signAndBroadcastProto(
+        [msg],
+        fee(data.denom, data.feeAmount, 260000),
+        data.rpc
+      );
+      if (result?.code === 0) {
+        dispatch(
+          setTxHash({
+            hash: result?.transactionHash,
+          })
+        );
+        return fulfillWithValue({ txHash: result?.transactionHash });
+      } else {
+        dispatch(
+          setError({
+            type: "error",
+            message: result?.rawLog,
+          })
+        );
+        return rejectWithValue(result?.rawLog);
+      }
+    } catch (error) {
+      dispatch(
+        setError({
+          type: "error",
+          message: error.message,
+        })
+      );
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 export const groupSlice = createSlice({
   name: "group",
   initialState,
@@ -85,6 +139,17 @@ export const groupSlice = createSlice({
         console.log(action.error);
         state.groups.member.status = "idle";
         // TODO: handle error
+      });
+
+    builder
+      .addCase(txCreateGroup.pending, (state) => {
+        state.tx.status = `pending`;
+      })
+      .addCase(txCreateGroup.fulfilled, (state, _) => {
+        state.tx.status = `idle`;
+      })
+      .addCase(txCreateGroup.rejected, (state, _) => {
+        state.tx.status = `rejected`;
       });
   },
 });
