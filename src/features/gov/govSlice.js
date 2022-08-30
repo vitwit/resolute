33902980130673e9e1f108d8ffc2a650bf1c01c9
aleem1314 <aleem@vitwit.com>
2022-08-30 17:40:23 +0530

    wip

--- src/features/gov/govSlice.js
+++ src/features/gov/govSlice.js
@@ -1,47 +1,73 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fee, signAndBroadcastAmino } from '../../txns/execute';
-import { GovVoteMsg } from '../../txns/gov';
-import { setError, setTxHash } from '../common/commonSlice';
-import govService from './govService';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fee, signAndBroadcastAmino } from "../../txns/execute";
+import { GovVoteMsg } from "../../txns/gov";
+import { setError, setTxHash } from "../common/commonSlice";
+import govService from "./govService";
 
 const initialState = {
   active: {
     proposals: [],
-    status: 'idle',
-    errMsg: '',
+    status: "idle",
+    errMsg: "",
   },
   tally: {
-    status: 'idle',
-    errMsg: '',
+    status: "idle",
+    errMsg: "",
     proposalTally: {},
   },
   votes: {
-    status: 'idle',
-    errMsg: '',
+    status: "idle",
+    errMsg: "",
     proposals: {},
   },
   tx: {
-    status: ''
-  }
+    status: "",
+  },
+  proposalInfo: {
+    status: "idle",
+    error: "",
+    proposalInfo: {},
+  },
 };
 
-export const getProposals = createAsyncThunk(
-  'gov/active-proposals',
-  async (data, {dispatch}) => {
-    const response = await govService.proposals(data.baseURL, data.key, data.limit, 2);
-    if (response?.data?.proposals?.length > 0) {
-      const proposals = response?.data?.proposals
-      for(let i = 0;i < proposals.length;i++) {
-        dispatch(getProposalTally({
-          baseURL: data.baseURL,
-          proposalId: proposals[i].proposal_id
-        }))
+export const getProposal = createAsyncThunk(
+  "gov/proposal-info",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await govService.proposal(data.baseURL, data.proposalId);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
-        dispatch(getVotes({
-          baseURL: data.baseURL,
-          proposalId: proposals[i].proposal_id,
-          voter: data.voter
-        }))
+export const getProposals = createAsyncThunk(
+  "gov/active-proposals",
+  async (data, { dispatch }) => {
+    const response = await govService.proposals(
+      data.baseURL,
+      data.key,
+      data.limit,
+      2
+    );
+    if (response?.data?.proposals?.length > 0) {
+      const proposals = response?.data?.proposals;
+      for (let i = 0; i < proposals.length; i++) {
+        dispatch(
+          getProposalTally({
+            baseURL: data.baseURL,
+            proposalId: proposals[i].proposal_id,
+          })
+        );
+
+        dispatch(
+          getVotes({
+            baseURL: data.baseURL,
+            proposalId: proposals[i].proposal_id,
+            voter: data.voter,
+          })
+        );
       }
     }
     return response.data;
@@ -49,124 +75,152 @@ export const getProposals = createAsyncThunk(
 );
 
 export const getProposalTally = createAsyncThunk(
-  'gov/proposal-tally',
+  "gov/proposal-tally",
   async (data) => {
     const response = await govService.tally(data.baseURL, data.proposalId);
-    response.data.tally.proposal_id = data.proposalId
+    response.data.tally.proposal_id = data.proposalId;
     return response.data;
   }
 );
 
-export const getVotes = createAsyncThunk(
-  'gov/voter-votes',
-  async (data) => {
-    const response = await govService.votes(data.baseURL,data.proposalId, data.voter, data.key, data.limit);
-    response.data.vote.proposal_id = data.proposalId
-    return response.data;
-  }
-);
+export const getVotes = createAsyncThunk("gov/voter-votes", async (data) => {
+  const response = await govService.votes(
+    data.baseURL,
+    data.proposalId,
+    data.voter,
+    data.key,
+    data.limit
+  );
+  response.data.vote.proposal_id = data.proposalId;
+  return response.data;
+});
 
 export const txVote = createAsyncThunk(
-  'gov/tx-vote',
+  "gov/tx-vote",
   async (data, { rejectWithValue, fulfillWithValue, dispatch }) => {
     try {
-      const msg = GovVoteMsg(data.proposalId, data.voter, data.option)
-      const result = await signAndBroadcastAmino([msg], fee(data.denom, data.feeAmount, 260000),data.chainId, data.rpc)
+      const msg = GovVoteMsg(data.proposalId, data.voter, data.option);
+      const result = await signAndBroadcastAmino(
+        [msg],
+        fee(data.denom, data.feeAmount, 260000),
+        data.chainId,
+        data.rpc
+      );
       if (result?.code === 0) {
-        dispatch(setTxHash({
-          hash: result?.transactionHash
-        }))
-        return fulfillWithValue({txHash: result?.transactionHash});
-        } else {
-          dispatch(setError({
-            type: 'error',
-            message: result?.rawLog
-          }))
-          return rejectWithValue(result?.rawLog);
-        }
+        dispatch(
+          setTxHash({
+            hash: result?.transactionHash,
+          })
+        );
+        return fulfillWithValue({ txHash: result?.transactionHash });
+      } else {
+        dispatch(
+          setError({
+            type: "error",
+            message: result?.rawLog,
+          })
+        );
+        return rejectWithValue(result?.rawLog);
+      }
     } catch (error) {
-      dispatch(setError({
-        type: 'error',
-        message: error.message
-      }))
-      return rejectWithValue(error.message)
+      dispatch(
+        setError({
+          type: "error",
+          message: error.message,
+        })
+      );
+      return rejectWithValue(error.message);
     }
   }
 );
 
-
 export const proposalsSlice = createSlice({
-  name: 'gov',
+  name: "gov",
   initialState,
   reducers: {
     resetTx: (state) => {
       state.tx = {
-        status: '',
-      }
+        status: "",
+      };
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getProposals.pending, (state) => {
-        state.active.status = 'pending';
-        state.active.errMsg = ''
+        state.active.status = "pending";
+        state.active.errMsg = "";
       })
       .addCase(getProposals.fulfilled, (state, action) => {
-        state.active.status = 'idle';
-        state.active.proposals = action.payload?.proposals
-        state.active.pagination = action.payload?.pagination
-        state.active.errMsg = ''
+        state.active.status = "idle";
+        state.active.proposals = action.payload?.proposals;
+        state.active.pagination = action.payload?.pagination;
+        state.active.errMsg = "";
       })
       .addCase(getProposals.rejected, (state, action) => {
-        state.active.status = 'rejected';
-        state.active.errMsg = action.error.message
-      })
+        state.active.status = "rejected";
+        state.active.errMsg = action.error.message;
+      });
 
-      // tally
-      builder
+    // tally
+    builder
       .addCase(getProposalTally.pending, (state) => {
-        state.tally.status = 'pending';
-        state.tally.errMsg = ''
+        state.tally.status = "pending";
+        state.tally.errMsg = "";
       })
       .addCase(getProposalTally.fulfilled, (state, action) => {
-        state.tally.status = 'idle';
-        state.tally.proposalTally[action.payload?.tally?.proposal_id] = action.payload?.tally
-        state.tally.errMsg = ''
+        state.tally.status = "idle";
+        state.tally.proposalTally[action.payload?.tally?.proposal_id] =
+          action.payload?.tally;
+        state.tally.errMsg = "";
       })
       .addCase(getProposalTally.rejected, (state, action) => {
-        state.tally.status = 'rejected';
-        state.tally.errMsg = action.error.message
-      })
+        state.tally.status = "rejected";
+        state.tally.errMsg = action.error.message;
+      });
 
-      // votes
-      builder
+    // votes
+    builder
       .addCase(getVotes.pending, (state) => {
-        state.votes.status = 'pending';
-        state.votes.errMsg = ''
+        state.votes.status = "pending";
+        state.votes.errMsg = "";
       })
       .addCase(getVotes.fulfilled, (state, action) => {
-        state.votes.status = 'idle';
-        state.votes.proposals[action.payload?.vote?.proposal_id] = action.payload?.vote
-        state.votes.errMsg = ''
+        state.votes.status = "idle";
+        state.votes.proposals[action.payload?.vote?.proposal_id] =
+          action.payload?.vote;
+        state.votes.errMsg = "";
       })
       .addCase(getVotes.rejected, (state, action) => {
-        state.votes.status = 'rejected';
-        state.votes.errMsg = action.error.message
-      })
+        state.votes.status = "rejected";
+        state.votes.errMsg = action.error.message;
+      });
 
-      // tx-vote
-      builder
+    // tx-vote
+    builder
       .addCase(txVote.pending, (state) => {
-        state.tx.status = 'pending';
-
+        state.tx.status = "pending";
       })
       .addCase(txVote.fulfilled, (state, _) => {
-        state.tx.status = 'idle';
+        state.tx.status = "idle";
       })
       .addCase(txVote.rejected, (state, _) => {
-        state.tx.status = 'rejected';
-      })
+        state.tx.status = "rejected";
+      });
 
+    // proposal-info
+    builder
+      .addCase(getProposal.pending, (state) => {
+        state.proposalInfo.status = "pending";
+      })
+      .addCase(getProposal.fulfilled, (state, payload) => {
+        state.proposalInfo.status = "idle";
+        state.proposalInfo.proposalInfo = payload.payload.proposal;
+      })
+      .addCase(getProposal.rejected, (state, payload) => {
+        state.proposalInfo.status = "rejected";
+        state.proposalInfo.error =
+          payload?.payload?.message || payload.error.message;
+      });
   },
 });
 
--- src/pages/ProposalInfo.js
+++ src/pages/ProposalInfo.js
@@ -1,103 +1,164 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, Paper, Typography } from "@mui/material";
 import { getProposalComponent } from "./../utils/util";
+import { getLocalTime } from "./../utils/datetime";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getProposal } from "../features/gov/govSlice";
+import { parseBalance } from "../utils/denom";
+import { setError } from "../features/common/commonSlice";
 
 export default function ProposalInfo() {
+  const { id } = useParams();
+  const proposalState = useSelector((state) => state.gov.proposalInfo);
+  const dispatch = useDispatch();
+  const { proposalInfo } = proposalState;
+
+  const chainInfo = useSelector((state) => state.wallet.chainInfo);
+  useEffect(() => {
+    dispatch(
+      getProposal({
+        baseURL: chainInfo.config.rest,
+        proposalId: id,
+      })
+    );
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    if (proposalState.status === "rejected" && proposalState.error.length > 0) {
+      dispatch(
+        setError({
+          type: "error",
+          message: proposalState.error,
+        })
+      );
+    }
+  }, [proposalState]);
+
   return (
     <>
-      <Paper
-        sx={{
-          borderRadius: 0,
-          p: 2,
-          m: 2,
-          textAlign: "left",
-        }}
-        elevation={0}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography variant="h6" color="text.primary" gutterBottom>
-            #100
+      {proposalState.status === "idle" ? (
+        <>
+          <Typography
+            gutterBottom
+            sx={{
+              textAlign: "left",
+              pl: 2,
+              mt: 1,
+              mb: 1,
+            }}
+            color="text.primary"
+            variant="h6"
+            fontWeight={500}
+          >
+            Proposal Details
           </Typography>
-          {getProposalComponent("PROPOSAL_STATUS_VOTING_PERIOD")}
-        </div>
-        <Typography
-          variant="body1"
-          color="text.primary"
-          fontWeight={500}
-          gutterBottom
-        >
-          #75 Establishing a definition of NoWithVeto
-        </Typography>
+          <Paper
+            sx={{
+              borderRadius: 0,
+              p: 2,
+              m: 2,
+              textAlign: "left",
+            }}
+            elevation={0}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h6" color="text.primary" gutterBottom>
+                #{id}
+              </Typography>
+              {getProposalComponent(proposalInfo?.status)}
+            </div>
+            <Typography
+              variant="body1"
+              color="text.primary"
+              fontWeight={500}
+              gutterBottom
+            >
+              {proposalInfo?.content?.title}
+            </Typography>
 
-        <Grid container
-          sx={{
-            mt: 1,
-            mb: 1,
-          }}
-        >
-          <Grid item xs={6} md={4}>
-            <Typography gutterBottom color="text.secondary" variant="body1">
-              Submitted Time
-            </Typography>
-            <Typography gutterBottom color="text.primary" variant="body1">
-              2022-08-25 06:53
-            </Typography>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Typography gutterBottom color="text.secondary" variant="body1">
-              Type
-            </Typography>
-            <Typography gutterBottom color="text.primary" variant="body1">
-              TextProposal
-            </Typography>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Typography gutterBottom color="text.secondary" variant="body1">
-              Total Deposit
-            </Typography>
-            <Typography gutterBottom color="text.primary" variant="body1">
-              1000STAKE
-            </Typography>
-          </Grid>
+            <Grid
+              container
+              sx={{
+                mt: 1,
+                mb: 1,
+              }}
+            >
+              <Grid item xs={6} md={4}>
+                <Typography gutterBottom color="text.secondary" variant="body1">
+                  Submitted Time
+                </Typography>
+                <Typography gutterBottom color="text.primary" variant="body1">
+                  2022-08-25 06:53
+                </Typography>
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <Typography gutterBottom color="text.secondary" variant="body1">
+                  Type
+                </Typography>
+                <Typography gutterBottom color="text.primary" variant="body1">
+                  TextProposal
+                </Typography>
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <Typography gutterBottom color="text.secondary" variant="body1">
+                  Total Deposit
+                </Typography>
+                <Typography gutterBottom color="text.primary" variant="body1">
+                  {proposalInfo?.total_deposit?.length > 0
+                    ? parseBalance(proposalInfo?.total_deposit, 6, "stake")
+                    : "-"}
+                </Typography>
+              </Grid>
 
-          <Grid item xs={6} md={4}>
-            <Typography gutterBottom color="text.secondary" variant="body1">
-              Voting Start Time
-            </Typography>
-            <Typography gutterBottom color="text.primary" variant="body1">
-              2022-08-25 06:53
-            </Typography>
-          </Grid>
+              <Grid item xs={6} md={4}>
+                <Typography gutterBottom color="text.secondary" variant="body1">
+                  Voting Start Time
+                </Typography>
+                <Typography gutterBottom color="text.primary" variant="body1">
+                  {getLocalTime(proposalInfo?.voting_start_time)}
+                </Typography>
+              </Grid>
 
-          <Grid item xs={6} md={4}>
-            <Typography gutterBottom color="text.secondary" variant="body1">
-              Voting End Time
+              <Grid item xs={6} md={4}>
+                <Typography gutterBottom color="text.secondary" variant="body1">
+                  Voting End Time
+                </Typography>
+                <Typography gutterBottom color="text.primary" variant="body1">
+                  {getLocalTime(proposalInfo?.voting_end_time)}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Typography
+              color="text.primary"
+              variant="body1"
+              fontWeight={500}
+              gutterBottom
+            >
+              Proposal Details
             </Typography>
-            <Typography gutterBottom color="text.primary" variant="body1">
-              2022-08-25 06:53
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography
-          color="text.primary"
-          variant="body1"
-          fontWeight={500}
-        >
-          Proposal Details
-        </Typography>
-        <pre>
-        This proposal aims to come to consensus that we should delete the blocked.go 
-        file in the Osmosis Github Repository (https://github.com/osmosis-labs/osmosis/blob/main/app/blocked.go).
-         My name is Anthony Rosa, and I am the project lead at Moultrie Audits. Centralized entities like Tether are holding 
-         firm to protect censorship resistance (https://twitter.com/Tether_to/status/1562468486886346753?s=20&t=R3Gc5J6rUnSfOzeYxNZyuw). There are many aspects to this conversation, and it is encouraged to read through the associated commonwealth threads (linked below) prior to voting. The blocked.go file prevents users from withdrawing to the contained blocked addresses, and the file comments state “we block all OFAC-blocked ETH addresses from receiving tokens.” If we accept as a community that we should censor addresses the government deems illegitimate, our entire purpose becomes questionable.
-        </pre>
-      </Paper>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: parseDescription(
+                  `${proposalInfo?.content?.description}`
+                ),
+              }}
+            />
+          </Paper>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
+
+const parseDescription = (description) =>
+  description.replace(/(\r\n|\r|\n)/g, "<br />");
