1d207d2a3802458701c5910b4229637529b2ffab
hemanthghs <ghshemanth@gmail.com>
2023-05-25 17:00:38 +0530

    refactor: migrate unjail validator page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -17,6 +17,7 @@ import { useSelector } from "react-redux";
 import OverviewPage from "./OverviewPage";
 import SendPage from "./SendPage";
 import ProposalInfo from "./gov/ProposalInfo";
+import UnjailPage from "./slashing/UnjailPage";
 
 export const ContextData = React.createContext();
 
@@ -166,6 +167,10 @@ export default function Home() {
 
           <Route path="/:networkName/staking" element={
             <StakingPage />
+          } />
+
+          <Route path="/:networkName/slashing" element={
+            <UnjailPage />
           } />
 
           <Route path="*" element={<Page404 />}></Route>
