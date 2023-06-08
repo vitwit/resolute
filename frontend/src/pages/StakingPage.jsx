972463085e4ec84ae6902023d9f91ef318ead7e1
saiteja <tejargukt123@gmail.com>
2023-06-08 16:15:06 +0530

    start

--- frontend/src/pages/StakingPage.jsx
+++ frontend/src/pages/StakingPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import SelectNetwork from '../components/common/SelectNetwork'
 import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function MultisigPage() {
     const networks = useSelector((state) => state.wallet.networks);
     const chainIDs = Object.keys(networks);
-
+    const param = useParams();
+    const naviagte = useNavigate();
     return (
         <div>
             <Box
