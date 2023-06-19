935c4db7123dd02a77ad0380ec8dceac34e9305d
saiteja <tejargukt123@gmail.com>
2023-06-19 15:16:49 +0530

    chore: small in url

--- frontend/src/features/common/commonService.ts
+++ frontend/src/features/common/commonService.ts
@@ -3,12 +3,12 @@ import Axios, { AxiosResponse } from "axios";
 const BASE_URL = process.env.REACT_APP_API_URI;
 
 const fetchPriceInfo = (denom: string): Promise<AxiosResponse> => {
-  const uri = `${BASE_URL}tokens-info/${denom}`;
+  const uri = `${BASE_URL}/tokens-info/${denom}`;
   return Axios.get(uri);
 };
 
 const fetchAllTokensPriceInfo = (): Promise<AxiosResponse> => {
-  const uri = `${BASE_URL}tokens-info`;
+  const uri = `${BASE_URL}/tokens-info`;
   return Axios.get(uri);
 };
 
