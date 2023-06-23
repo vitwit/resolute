6e886f158c19b7fd682eeba8fcba73e20e3ed407
saiteja <tejargukt123@gmail.com>
2023-06-23 15:37:18 +0530

    refactor : removed unnecessary resetDefaultStates in stakingPage

--- frontend/src/pages/StakingPage.jsx
+++ frontend/src/pages/StakingPage.jsx
@@ -1,30 +1,20 @@
-import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import SelectNetwork from "../components/common/SelectNetwork";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { Paper, Typography } from "@mui/material";
 import Validators from "./Validators";
-import { resetDefaultState as stakingResetDefaultState } from "../features/staking/stakeSlice";
-import { resetDefaultState as distributionResetDefaultState } from "../features/distribution/distributionSlice";
 import StakingOverview from "./stakingOverview/StakingOverview";
 
 export default function StakingPage() {
   const wallet = useSelector((state) => state.wallet);
   const stakingChains = useSelector((state) => state.staking.chains);
-  const networks = useSelector((state) => state.wallet.networks);
   const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
   const nameToChainIDs = wallet.nameToChainIDs;
   let currentNetwork = params.networkName;
   const defaultChain = "cosmoshub";
-
-  useEffect(() => {
-    let networkIDs = Object.keys(networks);
-    dispatch(stakingResetDefaultState(networkIDs));
-    dispatch(distributionResetDefaultState(networkIDs));
-  }, [wallet]);
 
   const handleOnSelect = (chainName) => {
     navigate(`/${chainName}/staking`);
