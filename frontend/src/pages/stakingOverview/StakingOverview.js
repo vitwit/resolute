21b0faa71e56e91968db42931c497c8d12ca640c
Teja2045 <106052623+Teja2045@users.noreply.github.com>
2023-07-06 10:28:14 +0530

    feat : add overview page actions (#543)
    * core: cleanup
    
    * feat: add restake button
    
    * feat: add restake transaction
    
    * completed the overview actions
    
    * chore: review change
    
    ---------
    
    Co-authored-by: aleem1314 <aleem@vitwit.com>

--- frontend/src/pages/stakingOverview/StakingOverview.js
+++ frontend/src/pages/stakingOverview/StakingOverview.js
@@ -7,8 +7,7 @@ import { getDelegatorTotalRewards } from '../../features/distribution/distributi
 import { Chain } from './Chain';
 import { getTokenPrice } from "../../features/common/commonSlice";
 
-const StakingOverview = (props) => {
-
+const StakingOverview = () => {
   const dispatch = useDispatch();
   const wallet = useSelector((state) => state.wallet);
   const chainsmap = useSelector((state) => state.staking.chains);
@@ -40,25 +39,29 @@ const StakingOverview = (props) => {
       let totalRewards = rewardchainsMap?.[chainIds[i]]?.delegatorRewards?.totalRewards / 10 ** decimal;
       totalRewardsInDollars += convertToDollars(coinMinimalDenom, +totalRewards);
 
-      let validatorRewards = rewardchainsMap?.[chainIds[i]]?.delegatorRewards?.list;
+      const validatorRewards = rewardchainsMap?.[chainIds[i]]?.delegatorRewards?.list;
 
-      if (!validatorRewards || validatorRewards.length == 0) continue;
+      if (!validatorRewards || validatorRewards.length === 0) continue;
       let validatorMap = {};
 
       for (let j = 0; j < validatorRewards.length; j++) {
         let address = validatorRewards?.[j]?.validator_address;
         let rewards = validatorRewards?.[j].reward;
         let validatorReward = 0;
+
         for (let k = 0; k < rewards.length; k++) {
           validatorReward += ((+rewards[k].amount) / 10 ** decimal);
         }
+
         validatorMap[address] = validatorReward.toFixed(3);
       }
+
       chainsData[chainIds[i]] = {
         totalRewards: totalRewards.toFixed(3),
         validators: validatorMap,
       }
     }
+
     chainsData.totalReward = totalRewardsInDollars;
     return chainsData;
   }
