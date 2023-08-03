7fac525260346342d1ed604208697bf9b8f11cd1
hemanthghs <ghshemanth@gmail.com>
2023-08-03 12:59:10 +0530

    ui: add connect wallet button

--- frontend/src/components/ConnectWallet.jsx
+++ frontend/src/components/ConnectWallet.jsx
@@ -1,23 +1,49 @@
-import React from "react"
-import { Typography } from "@mui/material"
-import { Box } from "@mui/system"
+import React, { useState } from "react";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { ConnectWalletDialog } from "./wallet/ConnectWallet";
+import { useDispatch } from "react-redux";
+import { connectWallet } from "./CustomAppBar";
+import { useTheme } from "@emotion/react";
 
 export default function ConnectWallet() {
+  const [showSelectWallet, setShowSelectWallet] = useState(false);
+  const dispatch = useDispatch();
 
-    return (
-        <Box
-            sx={{
-                mt: 4,
-            }}
-        >
-            <Typography
-                variant="h5"
-                gutterBottom
-                fontWeight={600}
-                color="text.primary"
-            >
-                Wallet not connected
-            </Typography>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        mt: 4,
+      }}
+    >
+      <Typography
+        variant="h5"
+        gutterBottom
+        fontWeight={600}
+        color="text.primary"
+      >
+        Wallet not connected
+      </Typography>
+      <Button
+        onClick={() => {
+          setShowSelectWallet(!showSelectWallet);
+        }}
+        sx={{
+          textTransform: "none",
+        }}
+        variant="contained"
+        disableElevation
+      >
+        Connect wallet
+      </Button>
+      <ConnectWalletDialog
+        open={showSelectWallet}
+        onClose={() => setShowSelectWallet(false)}
+        onWalletSelect={(wallet) => {
+          connectWallet(wallet, dispatch);
+          setShowSelectWallet(false);
+        }}
+      />
+    </Box>
+  );
 }
