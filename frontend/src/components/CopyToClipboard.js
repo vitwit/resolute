e7e63949dc26113559e874dc719d1728e9e5ef91
saiteja <tejargukt123@gmail.com>
2023-08-04 15:07:42 +0530

    feat: copy address to clipboard

---
+++ frontend/src/components/CopyToClipboard.js
@@ -0,0 +1,37 @@
+import ContentCopyOutlined from "@mui/icons-material/ContentCopyOutlined";
+import { Box, Chip, IconButton, Tooltip } from "@mui/material";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { copyToClipboard } from "../utils/clipboard";
+import { shortenAddress } from "../utils/util";
+
+export const CopyToClipboard = (props) => {
+  const { message } = props;
+  const toolTipEnabled = props.toolTipEnabled;
+  const dispatch = useDispatch();
+  return (
+    <Box>
+      {toolTipEnabled ? (
+        <Tooltip title="Copy address">
+          <IconButton
+            size="small"
+            onDelete={() => {
+              copyToClipboard(message, dispatch);
+            }}
+          >
+            <ContentCopyOutlined sx={{fontSize:12}}/>
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Chip
+          label={shortenAddress(message, 24)}
+          size="small"
+          deleteIcon={<ContentCopyOutlined />}
+          onDelete={() => {
+            copyToClipboard(message, dispatch);
+          }}
+        />
+      )}
+    </Box>
+  );
+};
