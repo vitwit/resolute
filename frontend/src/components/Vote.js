6b5a0fc9c565ff38ced4f70f4f8e572030ded806
hemanthghs <ghshemanth@gmail.com>
2023-08-04 16:04:44 +0530

    gov: change gov voting ui

--- frontend/src/components/Vote.js
+++ frontend/src/components/Vote.js
@@ -12,9 +12,9 @@ import { useSelector } from "react-redux";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { setError } from "../features/common/commonSlice";
@@ -27,11 +27,20 @@ VoteDialog.propTypes = {
   isAuthzMode: PropTypes.bool.isRequired,
 };
 
+const votingButtonStyle = {
+  borderRadius: 10,
+  height: "28px",
+  mr: 1,
+  textTransform: "none",
+  fontSize: "18px",
+};
 
 export default function VoteDialog(props) {
   const [option, setOption] = React.useState("");
   const dispatch = useDispatch();
-  const [granter, setGranter] = React.useState(props.granters.length > 0 ? props.granters[0] : "");
+  const [granter, setGranter] = React.useState(
+    props.granters.length > 0 ? props.granters[0] : ""
+  );
   const [justification, setJustification] = React.useState("");
   const govTx = useSelector((state) => state.gov.tx);
   const authzExecTx = useSelector((state) => state.authz.execTx);
@@ -42,13 +51,13 @@ export default function VoteDialog(props) {
   };
 
   const handleVote = () => {
-    if(!option) {
+    if (!option) {
       dispatch(
         setError({
           type: "error",
-          message: "Vote option not selected"
+          message: "Vote option not selected",
         })
-      )
+      );
       return;
     }
     props.onVote({
@@ -72,100 +81,82 @@ export default function VoteDialog(props) {
       >
         <DialogContent>
           <Box>
-            <FormControl
-              required
-              component="fieldset"
-              sx={{ m: 2 }}
-              variant="standard"
+            <Typography
+              variant="body1"
+              color="text.primary"
+              fontWeight={600}
+              sx={{ mb: 3 }}
             >
-              <Typography variant="body1" color="text.primary" fontWeight={600}>
-                Select Vote Option
-              </Typography>
-              <FormGroup
+              Select Vote Option
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "", mb: 2 }}>
+              <Button
+                variant={option === "yes" ? "contained" : "outlined"}
+                onClick={() => {
+                  setOption("yes");
+                }}
+                sx={votingButtonStyle}
+                disableElevation
+              >
+                Yes
+              </Button>
+              <Button
+                variant={option === "no" ? "contained" : "outlined"}
+                onClick={() => {
+                  setOption("no");
+                }}
+                sx={votingButtonStyle}
+                disableElevation
+              >
+                No
+              </Button>
+              <Button
+                variant={option === "noWithVeto" ? "contained" : "outlined"}
+                onClick={() => {
+                  setOption("noWithVeto");
+                }}
+                sx={votingButtonStyle}
+                disableElevation
+              >
+                NoWithVeto
+              </Button>
+              <Button
+                variant={option === "abstain" ? "contained" : "outlined"}
+                onClick={() => {
+                  setOption("abstain");
+                }}
+                sx={votingButtonStyle}
+                disableElevation
+              >
+                Abstain
+              </Button>
+            </Box>
+            {props.isAuthzMode && props.granters.length > 0 ? (
+              <FormControl
+                fullWidth
                 sx={{
-                  display: "flex",
-                  flexDirection: "initial",
-                  mt: 2,
+                  mt: 1,
                 }}
               >
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={option === "yes"}
-                      value="yes"
-                      onChange={handleChange}
-                      name="yes"
-                    />
-                  }
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={option === "no"}
-                      value="no"
-                      onChange={handleChange}
-                      name="no"
-                    />
-                  }
-                  label="No"
-                />
-
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={option === "noWithVeto"}
-                      value="noWithVeto"
-                      onChange={handleChange}
-                      name="noWithVeto"
-                    />
-                  }
-                  label="NoWithVeto"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={option === "abstain"}
-                      value="abstain"
-                      onChange={handleChange}
-                      name="abstain"
-                    />
-                  }
-                  label="Abstain"
-                />
-
-              </FormGroup>
-            </FormControl>
-            {
-              props.isAuthzMode && props.granters.length > 0 ?
-                <FormControl fullWidth
-                  sx={{
-                    mt: 1,
+                <InputLabel id="granter-label">Granter Account</InputLabel>
+                <Select
+                  labelId="granter-label"
+                  id="granter-select"
+                  value={granter}
+                  label="Granter account"
+                  onChange={(e) => {
+                    setGranter(e.target.value);
                   }}
+                  size="small"
                 >
-                  <InputLabel id="granter-label">Granter Account</InputLabel>
-                  <Select
-                    labelId="granter-label"
-                    id="granter-select"
-                    value={granter}
-                    label="Granter account"
-                    onChange={(e) => {
-                      setGranter(e.target.value);
-                    }}
-                    size="small"
-                  >
-                    {
-                      props.granters.map((granter, index) => (
-
-                        <MenuItem id={index} value={granter}>{granter}</MenuItem>
-                      ))
-                    }
-                  </Select>
-
-                </FormControl>
-                :
-                null
-            }
+                  {props.granters.map((granter, index) => (
+                    <MenuItem id={index} value={granter}>
+                      {granter}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            ) : null}
 
             <FormControl
               fullWidth
@@ -179,9 +170,8 @@ export default function VoteDialog(props) {
                 placeholder="justification"
                 value={justification}
                 onChange={(e) => {
-                  setJustification(e.target.value)
+                  setJustification(e.target.value);
                 }}
-
               />
             </FormControl>
           </Box>
@@ -196,7 +186,7 @@ export default function VoteDialog(props) {
             disableElevation
           >
             {govTx?.status === "pending" ||
-              authzExecTx?.status === "pending" ? (
+            authzExecTx?.status === "pending" ? (
               <CircularProgress size={25} />
             ) : (
               "Vote"
