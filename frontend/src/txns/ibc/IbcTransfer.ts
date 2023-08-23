61ed7ca243c5d7d67dc10c03938849264206e21b
saiteja <tejargukt123@gmail.com>
2023-08-23 11:12:17 +0530

    feat: added ibc transfer message

---
+++ frontend/src/txns/ibc/IbcTransfer.ts
@@ -0,0 +1,27 @@
+import { MsgTransfer } from "cosmjs-types/ibc/applications/transfer/v1/tx";
+import { Msg } from "../types";
+
+const msgIBCSendTypeUrl: string = "/ibc.applications.transfer.v1.MsgTransfer";
+
+export function IBCTransferMsg(
+  from: string,
+  to: string,
+  amount: number,
+  denom: string,
+  sourcePort: string,
+  sourceChannel: string
+): Msg {
+  return {
+    typeUrl: msgIBCSendTypeUrl,
+    value: MsgTransfer.fromPartial({
+      sender: from,
+      receiver: to,
+      token: {
+        denom: denom,
+        amount: String(amount),
+      },
+      sourceChannel: sourceChannel,
+      sourcePort: sourcePort,
+    }),
+  };
+}
