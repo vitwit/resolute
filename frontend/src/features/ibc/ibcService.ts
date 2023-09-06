0940c47c963d130dfe753dc2613b331e70559c7a
saiteja <tejargukt123@gmail.com>
2023-09-06 15:34:58 +0530

    feat: added query to fetch revision number and height details

--- frontend/src/features/ibc/ibcService.ts
+++ frontend/src/features/ibc/ibcService.ts
@@ -5,8 +5,14 @@ const fetchConsensusState = (rpcURL: string): Promise<AxiosResponse> => {
   return Axios.get(uri);
 };
 
+const fetchIBCRevisionInfo = (rpcURL: string): Promise<AxiosResponse> => {
+  let uri = `${rpcURL}ibc/core/client/v1/client_states`;
+  return Axios.get(uri);
+};
+
 const result = {
   consensusState: fetchConsensusState,
+  fetchIBCRevisionInfo: fetchIBCRevisionInfo,
 };
 
 export default result;
