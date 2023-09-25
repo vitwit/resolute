3c558237e93bb8b262064255754eca385130f3d2
saiteja <tejargukt123@gmail.com>
2023-09-25 15:00:03 +0530

    chore: rm file

--- passage.sh
+++
@@ -1,6 +0,0 @@
-passage init --chain-id testnet myvalidator
-passage add-genesis-account validator 1000000000upasg --keyring-backend test
-passage add-genesis-account account1 1000000000upasg --keyring-backend test
-passage add-genesis-account account2 1000000000upasg --keyring-backend test
-passage gentx validator 1000000000upasg --chain-id testnet
-passage collect-gentxs
