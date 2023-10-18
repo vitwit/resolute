0ce33896e4c47c767eab619d98f33eab63648a1d
saiteja <tejargukt123@gmail.com>
2023-10-18 15:11:39 +0530

    feegrant page - 1

--- frontend/src/app/(routes)/feegrant/page.tsx
+++ frontend/src/app/(routes)/feegrant/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Feegrants from "../../../components/tabs/Feegrants";
+import GrantsToMeTable from "staking/components/GrantsToMeTable";
 
 const page = () => {
   return (
-    <div className="page">
+    <div className="page space-y-10">
       <Feegrants />
+      <GrantsToMeTable />
     </div>
   );
 };
