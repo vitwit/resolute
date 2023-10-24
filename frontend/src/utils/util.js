a51e7fcd46b796085f5be4c6ae592f8b1a4b4948
saiteja <tejargukt123@gmail.com>
2023-10-24 15:38:58 +0530

    chore: fixed sidebar

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -59,6 +59,8 @@ export const getSelectedPartFromURL = (urlParts) => {
       return "Staking";
     case "feegrant":
       return "Feegrant";
+    case "governance":
+      return "Governance";
     case "groups":
       return "Groups";
     case "authz":
