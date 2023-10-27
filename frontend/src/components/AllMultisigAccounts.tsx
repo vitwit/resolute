e9a5a4a870cd5aac06573192f19edeb7024fb413
pavania1 <adinapavani@gmail.com>
2023-10-27 20:53:59 +0530

    added changes in Allmultisig accounts page

--- frontend/src/components/AllMultisigAccounts.tsx
+++ frontend/src/components/AllMultisigAccounts.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 
 const AllMultisigAccounts = () => {
+    const multisigData = [1,2,3,4];
   return (
     <div className='multisig-grid'>
-      <div className='multisig'>
+        {multisigData.map((index: number, id: number)=>(
+      <div className='multisig' key={id} >
         <div className='multisig-details'>
           <div className='multisig-first'>Name</div>
           <div className='multisig-second'>Multisign Transaction name</div>
@@ -39,6 +41,7 @@ const AllMultisigAccounts = () => {
           <div className='multisig-second'>2023-08-24 10:34:43</div>
         </div>
       </div>
+      ))}
     </div>
   );
 };
