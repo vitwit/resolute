fd12545b47ba0abdb665350b8741eeb3ed1e82ff
pavania1 <adinapavani@gmail.com>
2023-10-30 14:55:59 +0530

    created the multisig Popups

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,6 +20,7 @@ const OverviewPage = () => {
       <AccountDetails />
       <BalanceOverview />
       
+      
      
      
      
---
+++ frontend/src/components/popups/MemberslimireachedMultisig.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+function MemberslimireachedMultisig() {
+  return (
+    <div className='members-limit-reach '>
+    <div className='member-head'>
+        <h2>Member Limit Reached</h2>
+    </div>
+    <div className='member-image'>
+        <Image src="/member.png" width={100} height={100} alt="vote-image" />
+        <p>You have reached the limit to add the team members.</p>
+    </div>
+    <div className='member-button'>Dismiss</div>
+</div>
+  )
+}
+
+export default MemberslimireachedMultisig
