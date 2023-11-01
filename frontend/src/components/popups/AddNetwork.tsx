9fade5f85adfd5fd68cf4288ae8f62cfe82f3511
hemanthghs <ghshemanth@gmail.com>
2023-11-01 14:15:51 +0530

    wip

--- frontend/src/components/popups/AddNetwork.tsx
+++ frontend/src/components/popups/AddNetwork.tsx
@@ -1,8 +1,13 @@
 "use client";
 import React, { ChangeEvent, useState } from "react";
 import Image from "next/image";
-import { Button, Dialog, DialogContent } from "@mui/material";
+import { Dialog, DialogContent, IconButton } from "@mui/material";
+import { ValidationError, validate } from "jsonschema";
 import CustomTabsGroup from "../CustomTabsGroup";
+import ClearIcon from "@mui/icons-material/Clear";
+import networkConfigFormat from "../../utils/networkConfigFormat.json";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 
 const AddNetwork = ({
   open,
@@ -11,15 +16,27 @@ const AddNetwork = ({
   open: boolean;
   handleClose: () => void;
 }) => {
-  const [tab, setTab] = useState(0);
+  //TODO: Add url and store is contants file
   const ADD_NETWORK_TEMPLATE: string = "";
+
+  const [tab, setTab] = useState(0);
+  const [uploadedFileName, setUploadedFileName] = useState<string>("");
+  const [validationErrors, setValidationErrors] = useState<ValidationError[]>(
+    []
+  );
+  const [chainIDExist, setChainIDExist] = useState<boolean>(false);
+  const [chainNameExist, setChainNameExist] = useState<boolean>(false);
+  const [showErrors, setShowErrors] = useState<boolean>(false);
+
+  const nameToChainIDs = useSelector(
+    (state: any) => state.wallet.nameToChainIDs
+  );
 
   const handleTabChange = (value: number) => {
     setTab(value);
   };
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log("here...");
     const file = e.target.files?.[0];
     if (!file) {
       return;
@@ -28,6 +45,7 @@ const AddNetwork = ({
     reader.onload = (e) => {
       const contents = e.target?.result as string;
       onFileContents(contents);
+      setUploadedFileName(file.name);
     };
     reader.onerror = (e) => {
       alert(e);
@@ -37,7 +55,41 @@ const AddNetwork = ({
   };
 
   const onFileContents = (content: string): void => {
-    console.log(content);
+    try {
+      const parsedData = JSON.parse(content);
+      const res = validate(parsedData, networkConfigFormat);
+      setValidationErrors(res.errors);
+      if (!_.get(res, "errors.length")) {
+        setChainNameExist(
+          chainNameExists(_.get(parsedData, "config.chain_name"))
+        );
+        setChainIDExist(chainIDExists(_.get(parsedData, "config.chain_id")));
+      }
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const chainNameExists = (chainName: string) => {
+    const chainNamesList = Object.keys(nameToChainIDs);
+    if (chainNamesList.includes(chainName.toLowerCase())) {
+      return true;
+    }
+    return false;
+  };
+
+  const chainIDExists = (chainID: string) => {
+    const chainNamesList = Object.keys(nameToChainIDs);
+    for (let chain in chainNamesList) {
+      if (
+        nameToChainIDs[chainNamesList[chain]].toLowerCase() ===
+        chainID.toLowerCase()
+      ) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return (
@@ -73,13 +125,40 @@ const AddNetwork = ({
                 }}
               >
                 <div className="flex flex-col items-center justify-center">
-                  <Image
-                    src="./upload-icon.svg"
-                    width={60}
-                    height={60}
-                    alt="Upload file"
-                  />
-                  <div className="mt-2">Upload file here</div>
+                  {uploadedFileName ? (
+                    <>
+                      <div className="font-bold text-">
+                        {uploadedFileName}{" "}
+                        <IconButton
+                          aria-label="delete txn"
+                          color="error"
+                          sx={{
+                            mt: 2,
+                            mb: 2,
+                          }}
+                          onClick={(e) => {
+                            setUploadedFileName("");
+                            setChainIDExist(false);
+                            setChainNameExist(false);
+                            setShowErrors(false);
+                            e.stopPropagation();
+                          }}
+                        >
+                          <ClearIcon />
+                        </IconButton>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <Image
+                        src="./upload-icon.svg"
+                        width={60}
+                        height={60}
+                        alt="Upload file"
+                      />
+                      <div className="mt-2">Upload file here</div>
+                    </>
+                  )}
                 </div>
                 <input
                   id="multisig_file"
@@ -105,8 +184,44 @@ const AddNetwork = ({
                 </a>
               </div>
             </div>
+            {uploadedFileName && validationErrors?.length ? (
+              <div className="w-full">
+                <div className="flex gap-4">
+                  <div className="text-red-600 font-bold">
+                    Invalid json file
+                  </div>
+                  <div
+                    className="underline underline-offset-2 cursor-pointer text-[12px]"
+                    onClick={() => setShowErrors((showErrors) => !showErrors)}
+                  >
+                    show more
+                  </div>
+                </div>
+                {showErrors &&
+                  validationErrors?.map((item, index) => (
+                    <li key={index}>{item.stack}</li>
+                  ))}
+              </div>
+            ) : (
+              <div>
+                {chainNameExist ? (
+                  <div className="text-red-600 font-bold">
+                    Chain already exist with the given chain_name
+                  </div>
+                ) : (
+                  <></>
+                )}
+                {chainIDExist ? (
+                  <div className="text-red-600 font-bold">
+                    Chain already exist with the given chain_id
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
             <div className="w-full">
-                <button className="custom-btn w-full">Add Network</button>
+              <button className="custom-btn w-full">Add Network</button>
             </div>
           </div>
         </div>
