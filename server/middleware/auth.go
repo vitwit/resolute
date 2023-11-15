0fdfa4487920d104c2d06547e81b0fcfdbe59815
charymalloju <chary@vitwit.com>
2023-11-15 16:46:50 +0530

    renamed auth middleware file name

---
+++ server/middleware/auth.go
@@ -0,0 +1,100 @@
+package middleware
+
+import (
+	"database/sql"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"github.com/vitwit/resolute/server/model"
+)
+
+func (h *Handler) AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		signature := c.QueryParams().Get("signature")
+		address := c.QueryParams().Get("address")
+
+		if address == "" {
+			return c.JSON(http.StatusNotAcceptable, model.ErrorResponse{
+				Status:  "error",
+				Message: "address is required",
+			})
+		}
+
+		if signature == "" {
+			return c.JSON(http.StatusNotAcceptable, model.ErrorResponse{
+				Status:  "error",
+				Message: "signature is required",
+			})
+		}
+
+		var userAddess string
+
+		saniSignature := strings.Replace(signature, " ", "+", -1)
+
+		err := h.DB.QueryRow(`SELECT address FROM users where address=$1 and signature=$2`, address, saniSignature).Scan(&userAddess)
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "Unauthorized",
+				Message: err.Error(),
+			})
+		} else if err != nil {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "error",
+				Message: "failed to decode",
+				Log:     err.Error(),
+			})
+		}
+
+		return next(c)
+	}
+}
+
+func (h *Handler) IsMultisigAdmin(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		address := c.QueryParams().Get("address")
+
+		var userAddess string
+
+		err := h.DB.QueryRow(`SELECT address FROM multisig_accounts where created_by=$1`, address).Scan(&userAddess)
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "Unauthorized",
+				Message: err.Error(),
+			})
+		} else if err != nil {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "error",
+				Message: "failed to decode",
+				Log:     err.Error(),
+			})
+		}
+
+		return next(c)
+	}
+}
+
+func (h *Handler) IsMultisigMember(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		address := c.QueryParams().Get("address")
+		multisigAddress := c.Param("address")
+
+		var userAddess string
+
+		err := h.DB.QueryRow(`SELECT address FROM pubkeys where address=$1 and multisig_address=$2`, address, multisigAddress).Scan(&userAddess)
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "Unauthorized",
+				Message: "Your not member of the multisig",
+			})
+		} else if err != nil {
+			return c.JSON(http.StatusBadRequest, model.ErrorResponse{
+				Status:  "error",
+				Message: "failed to decode",
+				Log:     err.Error(),
+			})
+		}
+
+		return next(c)
+	}
+}
