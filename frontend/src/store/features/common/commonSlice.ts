b648c02ec1e03d4dd25717ec6f02e1fb3d005530
hemanthghs <ghshemanth@gmail.com>
2023-11-17 12:07:33 +0530

    Add types file

--- frontend/src/store/features/common/commonSlice.ts
+++ frontend/src/store/features/common/commonSlice.ts
@@ -3,57 +3,6 @@ import commonService from './commonService';
 import { AxiosError } from 'axios';
 import { ERR_UNKNOWN } from '../../../utils/errors';
 
-interface ErrorState {
-  message: string;
-  type: string;
-}
-
-interface TxSuccess {
-  hash: string;
-}
-
-interface TxLoadRes {
-  load: boolean;
-}
-
-interface TokenInfo {
-  usd: number;
-  usd_24h_change: number;
-}
-
-interface InfoState {
-  denom: string;
-  coingecko_name: string;
-  enabled: boolean;
-  last_updated: string;
-  info: TokenInfo;
-}
-
-interface TokensInfoState {
-  error: string;
-  info: InfoState;
-  status: string;
-}
-
-interface SelectedNetwork {
-  chainName: string;
-}
-
-interface AllTokensInfoState {
-  error: string;
-  info: Record<string, InfoState>;
-  status: string;
-}
-
-interface CommonState {
-  errState: ErrorState;
-  txSuccess: TxSuccess;
-  txLoadRes: TxLoadRes;
-  tokensInfoState: TokensInfoState;
-  selectedNetwork: SelectedNetwork;
-  allTokensInfoState: AllTokensInfoState;
-}
-
 const initialState: CommonState = {
   errState: {
     message: '',
