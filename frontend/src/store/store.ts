da4ca5047e125d387eb3d5e2b21f0ee8269c2ab4
hemanthghs <ghshemanth@gmail.com>
2023-11-17 11:59:02 +0530

    feat: Add common slice and common service

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,11 +3,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import multisigSlice from './features/multisig/multisigSlice';
 import walletSlice from './features/wallet/walletSlice';
+import commonSlice from './features/common/commonSlice';
 
 export const store = configureStore({
   reducer: {
     wallet: walletSlice,
     multisig: multisigSlice,
+    common: commonSlice,
   },
 });
 
