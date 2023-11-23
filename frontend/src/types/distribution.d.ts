c1b1bdd430cca5e6ca92fdedf0ef5c7bab201e9b
Hemanth Sai <ghshemanth@gmail.com>
2023-11-23 12:19:48 +0530

    feat: Add distribution slice (#891)
    * feat: Add distribution slice
    
    * review changes

---
+++ frontend/src/types/distribution.d.ts
@@ -0,0 +1,60 @@
+import { Pagination } from '../proposals';
+
+interface DelegatorTotalRewardsRequest {
+  baseURL: string;
+  address: string;
+  chainID: string;
+  denom?: string;
+  pagination?: Pagination;
+}
+
+interface Reward {
+  denom: string;
+  amount: string;
+}
+
+interface DelegatorRewards {
+  validator_address: string;
+  rewards: Reward[];
+}
+
+interface DefaultState {
+  delegatorRewards: {
+    list: DelegatorRewards[];
+    totalRewards: number;
+    status: string;
+    errMsg: string;
+    pagination: Pagination;
+  };
+  tx: {
+    status: string;
+    txHash: string;
+  };
+}
+
+interface DistributionStoreInitialState {
+  chains: {
+    [key: string]: DefaultState;
+  };
+  defaultState: DefaultState;
+}
+
+interface ChainsMap {
+  [key: string]: DefaultState;
+}
+
+interface DelegationsPairs {
+  validator: string;
+  delegator: string;
+}
+
+interface TxWithdrawAllRewardsInputs {
+  msgs: DelegationsPairs[];
+  denom: string;
+  chainID: string;
+  aminoConfig: AminoConfig;
+  prefix: string;
+  rest: string;
+  feeAmount: number;
+  feegranter: string;
+}
