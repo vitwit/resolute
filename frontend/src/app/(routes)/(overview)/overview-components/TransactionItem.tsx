229a8a013d250474a495473c5fb371e173034b82
saiteja <tejargukt123@gmail.com>
2023-11-27 18:27:23 +0530

    chore

--- frontend/src/app/(routes)/(overview)/overview-components/TransactionItem.tsx
+++ frontend/src/app/(routes)/(overview)/overview-components/TransactionItem.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image';
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const uiTx = formatTransaction(transaction);
-  
+
   return (
     <div className="w-full flex gap-4">
       <div className="flex gap-2 min-w-[88px] max-w-[88px]">
         <div className="space-y-4">
-          <div className="ml-auto text-right text-[12px] max-w-[68px]">{uiTx.time}</div>
+          <div className="ml-auto text-right text-[12px] max-w-[68px]">
+            {uiTx.time}
+          </div>
           <Image
             className="ml-auto"
             src="/back-arrow.svg"
@@ -27,7 +29,12 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
       <div className="space-y-2">
         <div className="formatted-text-1">{uiTx.firstMessage}</div>
         <div className="flex gap-2">
-          <Image width={20} height={20} alt="success" src="/round-checked.svg" />
+          <Image
+            width={20}
+            height={20}
+            alt="success"
+            src="/round-checked.svg"
+          />
           <div className="text-xs not-italic font-normal leading-4 tracking-[0.48px]">
             {uiTx.isTxSuccess ? 'Transaction Successful' : 'Transaction Failed'}
           </div>
@@ -35,7 +42,7 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
         <div className="flex gap-4">
           {uiTx.showMsgs[0] && <Chip msg={uiTx.showMsgs[0]} />}
           {uiTx.showMsgs[1] && <Chip msg={uiTx.showMsgs[1]} />}
-          {uiTx.showMsgs[2] && <FilledChip />}
+          {uiTx.showMsgs[2] && <FilledChip count={uiTx.showMsgs.length - 2} />}
         </div>
       </div>
     </div>
@@ -46,8 +53,8 @@ export const Chip = ({ msg }: { msg: string }) => {
   return <div className="chip bg-[#26233b]">{msg}</div>;
 };
 
-export const FilledChip = () => {
-  return <div className="chip fill">+ 5 more</div>;
+export const FilledChip = ({ count }: { count: number }) => {
+  return <div className="chip fill">+ {count} more</div>;
 };
 
 export default TransactionItem;
