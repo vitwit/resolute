1d9abacf03e3f98d03e5a140eed15c38cbc58d70
Hemanth Sai <ghshemanth@gmail.com>
2023-11-27 16:05:09 +0530

    feat(gov): Implement gov slice (#895)
    * wip(gov): gov slice
    
    * wip(gov): refactor
    
    * chore(gov)
    
    * gov: Add getDepositProps reducer
    
    * wip(gov)
    
    * wip(gov): add get proposal
    
    * (gov): add constants
    
    * feat(gov): Add txDeposit and txVote
    
    * chore(gov): review changes
    
    * refactor(gov): review changes

---
+++ frontend/src/types/gov.d.ts
@@ -0,0 +1,174 @@
+import { TxStatus } from './enums';
+
+interface GovProposal {
+  proposal_id: string;
+  content?: {
+    '@type': string;
+    title: string;
+    description: string;
+  };
+  status: string;
+  final_tally_result: {
+    yes: string;
+    abstain: string;
+    no: string;
+    no_with_veto: string;
+  };
+  submit_time: string;
+  deposit_end_time: string;
+  total_deposit: {
+    denom: string;
+    amount: string;
+  }[];
+  voting_start_time: string;
+  voting_end_time: string;
+}
+
+interface GetProposalsInVotingResponse {
+  proposals: GovProposal[];
+  pagination: GovPagination;
+}
+
+interface VoteOption {
+  option: string;
+  weight: string;
+}
+
+interface Vote {
+  proposal_id: number;
+  voter: string;
+  option: string;
+  options: VoteOption[];
+}
+
+interface ProposalVote {
+  vote: Vote;
+}
+
+interface VotesData {
+  status: TxStatus;
+  errMsg: string;
+  proposals: {
+    [key: string]: ProposalVote;
+  };
+}
+
+interface ProposalTally {
+  [key: string]: {
+    yes: string;
+    abstain: string;
+    no: string;
+    no_with_veto: string;
+    proposal_id: string;
+  };
+}
+
+interface GetProposalTallyResponse {
+  [key: string]: {
+    yes: string;
+    abstain: string;
+    no: string;
+    no_with_veto: string;
+  };
+}
+
+interface ProposalTallyData {
+  status: TxStatus;
+  errMsg: string;
+  proposalTally: ProposalTally;
+}
+
+interface DepositParams {
+  min_deposit: {
+    denom: string;
+    amount: string;
+  }[];
+  max_deposit_period: string;
+}
+
+interface TallyParams {
+  quorum: string;
+  threshold: string;
+  vote_threshold: string;
+}
+
+interface VotingParams {
+  voting_period: string;
+}
+
+interface GovParamsResponse {
+  voting_params: VotingParams;
+  deposit_params: DepositParams;
+  tally_params: TallyParams;
+}
+
+interface GetProposalsInVotingInputs {
+  baseURL: string;
+  chainID: string;
+  voter: string;
+  key?: string;
+  limit?: number;
+}
+
+interface GetProposalsInDepositInputs {
+  baseURL: string;
+  chainID: string;
+  key?: string;
+  limit?: number;
+}
+
+interface GetVotesInputs {
+  baseURL: string;
+  proposalId: number;
+  voter: string;
+  chainID: string;
+  key?: string;
+  limit?: number;
+}
+
+interface GetProposalTallyInputs {
+  baseURL: string;
+  proposalId: number;
+  chainID: string;
+}
+
+interface GetDepositParamsInputs {
+  baseURL: string;
+  chainID: string;
+}
+
+interface GetProposalInputs {
+  baseURL: string;
+  proposalId: number;
+  chainID: string;
+}
+
+interface TxVoteInputs {
+  voter: string;
+  proposalId: number;
+  option: number;
+  denom: string;
+  chainID: string;
+  rpc: string;
+  rest: string;
+  aminoConfig: AminoConfig;
+  prefix: string;
+  feeAmount: number;
+  feegranter: string;
+  justification?: string;
+}
+
+interface TxDepositInputs {
+  depositer: string;
+  proposalId: number;
+  amount: number;
+  denom: string;
+  chainID: string;
+  rpc: string;
+  rest: string;
+  aminoConfig: AminoConfig;
+  prefix: string;
+  feeAmount: number;
+  feegranter: string;
+  justification?: string;
+}
