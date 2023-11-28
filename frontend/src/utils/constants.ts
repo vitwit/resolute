418eec90da44327bafbce2a4bc70493e107b8960
Hemanth Sai <ghshemanth@gmail.com>
2023-11-28 11:57:25 +0530

    feat: Implement select network and add network (#899)
    * wip: Add select network dialog
    
    * chore
    
    * wip
    
    * wip: select network
    
    * wip: Add new network
    
    * wip(add network)
    
    * chore(add network): change network config schema
    
    * Add rounting
    
    * chore(select network)
    
    * chore(select-network)
    
    * chore(select-network): review changes
    
    * review changes

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const GAS_FEE = 860000;
+export const ADD_NETWORK_TEMPLATE_URL = "https://raw.githubusercontent.com/vitwit/resolute/b5d184c8da894b2fea0ed40e56a599a1d813c422/frontend/public/add-network-template.json";
 export const PROPOSAL_STATUS_VOTING_PERIOD = 'PROPOSAL_STATUS_VOTING_PERIOD';
