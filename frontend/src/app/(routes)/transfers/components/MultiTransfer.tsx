116af926780361a9b547edba269290020f8808fd
saiteja <tejargukt123@gmail.com>
2023-12-01 12:42:01 +0530

    chore: lint fix

--- frontend/src/app/(routes)/transfers/components/MultiTransfer.tsx
+++ frontend/src/app/(routes)/transfers/components/MultiTransfer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MultiTransfer = ({ chainID }: { chainID: string }) => {
   return (
     <div className="w-full h-full flex pt-32 justify-center">
-      - MultiTransfer -
+      - MultiTransfer {chainID}-
     </div>
   );
 };
