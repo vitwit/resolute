fe450deca5d69a6730dde25cec23e4b6a8a3a38e
saiteja <tejargukt123@gmail.com>
2023-12-13 14:39:17 +0530

    chore: layout change

--- frontend/src/app/(routes)/transfers/components/TransfersPage.tsx
+++ frontend/src/app/(routes)/transfers/components/TransfersPage.tsx
@@ -22,27 +22,27 @@ const TransfersPage = ({ chainIDs }: { chainIDs: string[] }) => {
 
   return (
     <div className="w-full flex justify-between max-h-screen text-white">
-      <div className="w-full page-padding space-y-6 overflow-y-scroll">
+      <div className="w-full page-padding overflow-y-scroll">
         <MainTopNav title="Transfers" />
-        <div className="flex flex-col rounded-2xl bg-[#0e0b26] p-6 space-y-6">
-          <div className="flex justify-between items-center">
-            <div className="text-base not-italic font-normal leading-[normal]">
-              {tab.current}
-            </div>
-
-            <button
-              className="secondary-custom-btn"
-              onClick={() => {
-                if (chainIDs.length > 1) {
-                  alert('Multi transfer is not available for All networks!');
-                  return;
-                }
-                changeTab(tab);
-              }}
-            >
-              {tab.to}
-            </button>
+        <div className="flex justify-between items-center mt-10">
+          <div className="text-base not-italic font-normal leading-[normal]">
+            {tab.current}
           </div>
+
+          <button
+            className="secondary-custom-btn"
+            onClick={() => {
+              if (chainIDs.length > 1) {
+                alert('Multi transfer is not available for All networks!');
+                return;
+              }
+              changeTab(tab);
+            }}
+          >
+            {tab.to}
+          </button>
+        </div>
+        <div className="flex flex-col rounded-2xl bg-[#0e0b26] p-6 space-y-6 mt-6">
           {tab.current === SINGLE_TAB_TEXT ? (
             <SingleTransfer chainIDs={chainIDs} />
           ) : (
