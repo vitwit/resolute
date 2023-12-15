b58a64a8c0b8fe19a63cc00a0bd3f49716fd4800
hemanthghs <ghshemanth@gmail.com>
2023-12-15 10:46:23 +0530

    chore(multisig): review changes

--- frontend/src/app/(routes)/multisig/Multisig.tsx
+++ frontend/src/app/(routes)/multisig/Multisig.tsx
@@ -1,8 +1,20 @@
 'use client';
+import TopNav from '@/components/TopNav';
 import React from 'react';
 
 const Multisig = () => {
-  return <div>Multisig</div>;
+  return (
+    <div className="h-screen flex flex-col p-6 pl-10">
+      <div className="w-full flex justify-between">
+        <h2 className="text-[20px] leading-normal font-normal">Multisig</h2>
+        <TopNav />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center gap-1">
+        <p>All Networks page is not supported for Multisig.</p>
+        <p>Please select a network.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Multisig;
