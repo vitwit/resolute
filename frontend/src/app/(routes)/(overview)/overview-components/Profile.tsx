4e5dd712a26a75b85088cd7fa76497c7e8fa3bc7
hemanthghs <ghshemanth@gmail.com>
2023-12-21 18:41:54 +0530

    chore(ui): common ui changes

--- frontend/src/app/(routes)/(overview)/overview-components/Profile.tsx
+++ frontend/src/app/(routes)/(overview)/overview-components/Profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
   const profileName = useAppSelector((state) => state.wallet.name);
   return (
     <Tooltip title={profileName} arrow placement='bottom'>
-    <div className="flex items-center space-x-2 cursor-pointer">
+    <div className="flex items-center space-x-2 cursor-default">
       <Image src="/profile.svg" width={36} height={36} alt="profile"></Image>
       <p className="text-white text-base not-italic font-normal max-w-[112px] leading-[normal truncate">
         {profileName}
