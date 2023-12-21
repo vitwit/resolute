220cbd806f7737968ff03eb79b06045c6ee01fae
Hemanth Sai <ghshemanth@gmail.com>
2023-12-21 08:14:02 +0530

    staking(ui): fix staking ui issues (#1000)
    * refactor(ui): staking ui changes
    
    * refactor(ui): staking ui issues
    
    * refactor(ui): staking ui changes

--- frontend/src/app/(routes)/staking/components/DialogUndelegate.tsx
+++ frontend/src/app/(routes)/staking/components/DialogUndelegate.tsx
@@ -4,17 +4,12 @@ import {
   formatUnbondingPeriod,
   parseDelegation,
 } from '@/utils/util';
-import {
-  Dialog,
-  DialogContent,
-} from '@mui/material';
+import { CircularProgress, Dialog, DialogContent } from '@mui/material';
 import Image from 'next/image';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import {
-  dialogBoxStyles,
-} from '../styles';
-import { CLOSE_ICON_PATH, STAKING_DIALOG_IMAGE_PATH } from '@/utils/constants';
+import { dialogBoxStyles } from '../styles';
+import { CLOSE_ICON_PATH } from '@/utils/constants';
 import AmountInputField from './AmountInputField';
 
 const DialogUndelegate = ({
@@ -41,15 +36,15 @@ const DialogUndelegate = ({
     reset,
   } = useForm({
     defaultValues: {
-      amount: 0,
+      amount: '',
     },
   });
 
-  const onSubmit = (data: { amount: number }) => {
+  const onSubmit = (data: { amount: string }) => {
     if (validator) {
       onUndelegate({
         validator: validator?.operator_address || '',
-        amount: data?.amount || 0,
+        amount: Number(data?.amount) || 0,
       });
     }
   };
@@ -64,9 +59,7 @@ const DialogUndelegate = ({
       <DialogContent sx={{ padding: 0 }}>
         <div className="max-w-[890px] text-white">
           <div className="px-10 py-6 pt-10 flex justify-end">
-            <div
-              onClick={handleClose}
-            >
+            <div onClick={handleClose}>
               <Image
                 className="cursor-pointer"
                 src={CLOSE_ICON_PATH}
@@ -76,13 +69,7 @@ const DialogUndelegate = ({
               />
             </div>
           </div>
-          <div className="mt-6 mb-[72px] flex gap-6 pr-10 pl-6 items-center">
-            <Image
-              src={STAKING_DIALOG_IMAGE_PATH}
-              height={360}
-              width={235}
-              alt="Undelegate"
-            />
+          <div className="mb-10 flex gap-6 px-10 items-center">
             <div className="flex flex-col gap-10 w-full">
               <h2 className="text-[20px] font-bold leading-3">
                 {validator?.description?.moniker || '-'}
@@ -106,7 +93,7 @@ const DialogUndelegate = ({
                     <div
                       className="font-medium leading-10 cursor-pointer hover:underline underline-offset-2"
                       onClick={() => {
-                        setValue('amount', delegationShare);
+                        setValue('amount', delegationShare.toString());
                       }}
                     >
                       {formatCoin(delegationShare, currency.coinDenom)}
@@ -115,7 +102,7 @@ const DialogUndelegate = ({
                 </div>
                 <div className="bg-[#FFFFFF0D] px-4 rounded-2xl opacity-80 py-4 w-full space-y-4">
                   <div className="flex gap-2 text-[14px]">
-                    <div className="w-[200px] font-light leading-[24px] my-auto">
+                    <div className="w-[200px] font-light leading-[24px]">
                       <p>Undelegating will lock</p>
                       <p>
                         your funds for {formatUnbondingPeriod(stakingParams)}{' '}
@@ -142,9 +129,17 @@ const DialogUndelegate = ({
                     displayDenom={currency.coinDenom}
                     errors={errors}
                   />
-                  <div className="mt-10 flex gap-10 items-center">
-                    <button type="submit" className="dialog-delegate-button">
-                      {loading === 'pending' ? 'Loading...' : 'Undelegate'}
+                  <div className="mt-6 flex gap-10 items-center">
+                    <button
+                      type="submit"
+                      className="dialog-delegate-button"
+                      disabled={loading === 'pending'}
+                    >
+                      {loading === 'pending' ? (
+                        <CircularProgress size={18} sx={{ color: 'white' }} />
+                      ) : (
+                        'Undelegate'
+                      )}
                     </button>
                     <button
                       type="button"
