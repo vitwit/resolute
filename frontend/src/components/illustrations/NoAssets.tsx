41555bc1d89fa612d8f2dd3a132680167f4b07ab
saiteja <tejargukt123@gmail.com>
2023-12-27 11:14:09 +0530

    disabled draggability

--- frontend/src/components/illustrations/NoAssets.tsx
+++ frontend/src/components/illustrations/NoAssets.tsx
@@ -4,14 +4,15 @@ import messages from '@/utils/messages.json';
 
 const NoAssets = () => {
   return (
-    <div className="flex flex-col flex-1 gap-6 items-center justify-center disable-draggable">
+    <div className="flex flex-col flex-1 gap-6 items-center justify-center">
       <Image
+        className="disable-draggable"
         src="/no-assets-illustration.png"
         width={400}
         height={236}
         alt="no assets"
       />
-      <div className="text-white text-center text-base italic font-extralight leading-[normal] flex justify-center opacity-50">
+      <div className="text-white text-center text-base italic font-extralight leading-[normal] flex justify-center opacity-50 disable-draggable">
         {messages.noAssets}
       </div>
     </div>
