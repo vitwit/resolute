921b6d3370872f87e2cb77a0cc13abfcd9e169de
pavania1 <adinapavani@gmail.com>
2023-12-27 10:38:15 +0530

    chore: added justify-between in rightoverview

--- frontend/src/app/(routes)/governance/RightOverview.tsx
+++ frontend/src/app/(routes)/governance/RightOverview.tsx
@@ -192,190 +192,193 @@ const RightOverview = ({
         </div>
         {proposalLoadingStatus !== 'pending' ? (
           <>
-            <div className="flex-1 flex flex-col space-y-16">
-              <div className="space-y-4 w-full">
-                <div className="space-y-3">
-                  <div className="flex justify-between w-full">
-                    <div className="flex space-x-2">
-                      <Image
-                        src={networkLogo}
-                        width={32}
-                        height={32}
-                        alt="Network Logo"
-                      />
-
-                      <p className="items-center flex space-x-1 text-[14px] font-bold">
-                        #{get(proposalInfo, 'proposal_id')}
-                      </p>
-                      <p className="proposal-text-extralight items-center flex">
-                        | Proposal
-                      </p>
-                    </div>
-                    <div className="flex items-center proposal-text-extralight">
-                      {status === 'active' ? (
-                        <>
-                          {`Voting ends in ${getTimeDifferenceToFutureDate(
-                            get(proposalInfo, 'voting_end_time')
-                          )}`}
-                        </>
-                      ) : (
-                        <>
-                          {`Deposit period ends in ${getTimeDifferenceToFutureDate(
-                            get(proposalInfo, 'deposit_end_time')
-                          )}`}
-                        </>
-                      )}
-                    </div>
-                  </div>
-                  <div className="font-bold text-base text-white">
-                    {get(proposalInfo, 'content.title') ||
-                      get(proposalInfo, 'content.@type')}
-                  </div>
-                </div>
-
-                <div className="space-y-6">
-                  <div className="proposal-text-normal">
-                    {truncatedDescription}
-                    {isDescriptionTruncated && '...'}
-                  </div>
-                  <div className="flex space-x-6">
-                    <button
-                      className="button"
-                      onClick={() => {
-                        if (isStatusVoting) {
-                          setIsVotePopupOpen(true);
-                        } else {
-                          setIsDepositPopupOpen(true);
-                        }
-                      }}
-                    >
-                      <p className="proposal-text-medium">
-                        {isStatusVoting ? 'Vote' : 'Deposit'}
-                      </p>
-                    </button>
-                    <div className="view-full">
-                      <Link href={`/governance/${chainName}/${proposalId}`}>
-                        View Full Proposal
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full">
-                <VotePopup
-                  chainID={chainID}
-                  votingEndsInDays={getTimeDifferenceToFutureDate(
-                    get(proposalInfo, 'voting_end_time')
-                  )}
-                  proposalId={proposalId}
-                  proposalname={get(proposalInfo, 'content.title')}
-                  open={isVotePopupOpen}
-                  onClose={handleCloseVotePopup}
-                  networkLogo={networkLogo}
-                />
-
-                <DepositPopup
-                  chainID={chainID}
-                  votingEndsInDays={getTimeDifferenceToFutureDate(
-                    get(proposalInfo, 'deposit_end_time')
-                  )}
-                  proposalId={proposalId}
-                  proposalname={get(proposalInfo, 'content.title')}
-                  onClose={handleCloseDepositPopup}
-                  open={isDepositPopupOpen}
-                  networkLogo={networkLogo}
-                />
-              </div>
-              {isStatusVoting ? (
+            <div className="flex justify-between h-full">
+              <div className="flex-1 flex flex-col justify-between">
                 <div className="space-y-4 w-full">
-                  <div className="vote-grid space-y-4">
-                    <div className="voting-view w-full">
-                      <div className="w-full  text-white flex flex-col justify-center items-center space-y-4">
-                        <div className="text-[20px]">Quorum</div>
+                  <div className="space-y-3">
+                    <div className="flex justify-between w-full">
+                      <div className="flex space-x-2">
+                        <Image
+                          src={networkLogo}
+                          width={32}
+                          height={32}
+                          alt="Network Logo"
+                        />
 
-                        {quorumPercent ? (
-                          <Tooltip title={`${quorumPercent}%`}>
-                            <div className="flex w-full flex-col">
-                              <div className="flex flex-col items-center">
-                                <div>
-                                  {quorumPercent}/{quorumRequired}
+                        <p className="items-center flex space-x-1 text-[14px] font-bold">
+                          #{get(proposalInfo, 'proposal_id')}
+                        </p>
+                        <p className="proposal-text-extralight items-center flex">
+                          | Proposal
+                        </p>
+                      </div>
+                      <div className="flex items-center proposal-text-extralight">
+                        {status === 'active' ? (
+                          <>
+                            {`Voting ends in ${getTimeDifferenceToFutureDate(
+                              get(proposalInfo, 'voting_end_time')
+                            )}`}
+                          </>
+                        ) : (
+                          <>
+                            {`Deposit period ends in ${getTimeDifferenceToFutureDate(
+                              get(proposalInfo, 'deposit_end_time')
+                            )}`}
+                          </>
+                        )}
+                      </div>
+                    </div>
+                    <div className="font-bold text-base text-white">
+                      {get(proposalInfo, 'content.title') ||
+                        get(proposalInfo, 'content.@type')}
+                    </div>
+                  </div>
+
+                  <div className="space-y-6">
+                    <div className="proposal-text-normal">
+                      {truncatedDescription}
+                      {isDescriptionTruncated && '...'}
+                    </div>
+                    <div className="flex space-x-6">
+                      <button
+                        className="button"
+                        onClick={() => {
+                          if (isStatusVoting) {
+                            setIsVotePopupOpen(true);
+                          } else {
+                            setIsDepositPopupOpen(true);
+                          }
+                        }}
+                      >
+                        <p className="proposal-text-medium">
+                          {isStatusVoting ? 'Vote' : 'Deposit'}
+                        </p>
+                      </button>
+                      <div className="view-full">
+                        <Link href={`/governance/${chainName}/${proposalId}`}>
+                          View Full Proposal
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <VotePopup
+                    chainID={chainID}
+                    votingEndsInDays={getTimeDifferenceToFutureDate(
+                      get(proposalInfo, 'voting_end_time')
+                    )}
+                    proposalId={proposalId}
+                    proposalname={get(proposalInfo, 'content.title')}
+                    open={isVotePopupOpen}
+                    onClose={handleCloseVotePopup}
+                    networkLogo={networkLogo}
+                  />
+
+                  <DepositPopup
+                    chainID={chainID}
+                    votingEndsInDays={getTimeDifferenceToFutureDate(
+                      get(proposalInfo, 'deposit_end_time')
+                    )}
+                    proposalId={proposalId}
+                    proposalname={get(proposalInfo, 'content.title')}
+                    onClose={handleCloseDepositPopup}
+                    open={isDepositPopupOpen}
+                    networkLogo={networkLogo}
+                  />
+                </div>
+
+                {isStatusVoting ? (
+                  <div className="space-y-4 w-full">
+                    <div className="vote-grid space-y-4">
+                      <div className="voting-view w-full">
+                        <div className="w-full  text-white flex flex-col justify-center items-center space-y-4">
+                          <div className="text-[20px]">Quorum</div>
+
+                          {quorumPercent ? (
+                            <Tooltip title={`${quorumPercent}%`}>
+                              <div className="flex w-full flex-col">
+                                <div className="flex flex-col items-center">
+                                  <div>
+                                    {quorumPercent}/{quorumRequired}
+                                  </div>
+                                  <div className="bg-[#f0f0f3] h-[10px] w-[2px]"></div>
                                 </div>
-                                <div className="bg-[#f0f0f3] h-[10px] w-[2px]"></div>
+                                <div className="bg-[#FFFFFF0D] w-full h-[10px] rounded-full">
+                                  <div className="bg-[#f0f0f3] h-[10px] w-[2px] absolute  flex left-[225px]"></div>
+                                  <div
+                                    style={{ width: `${quorumPercent}%` }}
+                                    className={`bg-[#2DC5A4] h-[10px] rounded-l-full`}
+                                  ></div>
+                                </div>
                               </div>
-                              <div className="bg-[#FFFFFF0D] w-full h-[10px] rounded-full">
-                                <div className="bg-[#f0f0f3] h-[10px] w-[2px] absolute  flex left-[225px]"></div>
-                                <div
-                                  style={{ width: `${quorumPercent}%` }}
-                                  className={`bg-[#2DC5A4] h-[10px] rounded-l-full`}
-                                ></div>
-                              </div>
-                            </div>
-                          </Tooltip>
-                        ) : null}
-                      </div>
-
-                      <div className="flex justify-between items-start w-full">
-                        {data.map((item, index) => (
-                          <div
-                            key={index}
-                            className="flex flex-col items-center gap-2"
-                          >
-                            <CustomPieChart
-                              value={parseInt(item.value)}
-                              color={item.color}
-                              label={item.label}
-                            />
-                            <div className="proposal-text-extralight">{`${Math.floor(
-                              parseFloat(item.value)
-                            )}% ${item.label}`}</div>
-                          </div>
-                        ))}
-                      </div>
-                      <div className="flex justify-between w-full">
-                        <div className="flex proposal-text-extralight">
-                          Submitted on {proposalSubmittedOn}
+                            </Tooltip>
+                          ) : null}
                         </div>
-                        <div className="flex space-x-2">
-                          <Image
-                            src="/done-icon.svg"
-                            width={16}
-                            height={16}
-                            alt="Quorum reached icon"
-                          />
+
+                        <div className="flex justify-between items-start w-full">
+                          {data.map((item, index) => (
+                            <div
+                              key={index}
+                              className="flex flex-col items-center gap-2"
+                            >
+                              <CustomPieChart
+                                value={parseInt(item.value)}
+                                color={item.color}
+                                label={item.label}
+                              />
+                              <div className="proposal-text-extralight">{`${Math.floor(
+                                parseFloat(item.value)
+                              )}% ${item.label}`}</div>
+                            </div>
+                          ))}
+                        </div>
+                        <div className="flex justify-between w-full">
                           <div className="flex proposal-text-extralight">
-                            {parseFloat(quorumPercent) >=
-                            parseFloat(quorumRequired)
-                              ? 'Quorum Reached'
-                              : 'Quorum Not Reached'}
+                            Submitted on {proposalSubmittedOn}
+                          </div>
+                          <div className="flex space-x-2">
+                            <Image
+                              src="/done-icon.svg"
+                              width={16}
+                              height={16}
+                              alt="Quorum reached icon"
+                            />
+                            <div className="flex proposal-text-extralight">
+                              {parseFloat(quorumPercent) >=
+                              parseFloat(quorumRequired)
+                                ? 'Quorum Reached'
+                                : 'Quorum Not Reached'}
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ) : (
-                <div className="flex flex-col space-y-20">
-                  <div className="bg-[#FFFFFF0D] rounded-2xl">
-                    <DepositProposalInfo chainID={chainID} />
+                ) : (
+                  <div className="flex flex-col space-y-20">
+                    <div className="bg-[#FFFFFF0D] rounded-2xl">
+                      <DepositProposalInfo chainID={chainID} />
+                    </div>
+                    <div className="bg-[#FFFFFF0D] rounded-2xl">
+                      <DepositProposalDetails
+                        submittedAt={getTimeDifferenceToFutureDate(
+                          get(proposalInfo, 'submit_time', '-'),
+                          true
+                        )}
+                        endsAt={getTimeDifferenceToFutureDate(
+                          get(proposalInfo, 'deposit_end_time', '-')
+                        )}
+                        depositrequired={formatCoin(
+                          depositRequired,
+                          currency.coinDenom
+                        )}
+                        proposalNetwork={getChainName(chainID)}
+                      />
+                    </div>
                   </div>
-                  <div className="bg-[#FFFFFF0D] rounded-2xl">
-                    <DepositProposalDetails
-                      submittedAt={getTimeDifferenceToFutureDate(
-                        get(proposalInfo, 'submit_time', '-'),
-                        true
-                      )}
-                      endsAt={getTimeDifferenceToFutureDate(
-                        get(proposalInfo, 'deposit_end_time', '-')
-                      )}
-                      depositrequired={formatCoin(
-                        depositRequired,
-                        currency.coinDenom
-                      )}
-                      proposalNetwork={getChainName(chainID)}
-                    />
-                  </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           </>
         ) : (
