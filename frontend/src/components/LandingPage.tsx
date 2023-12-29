31ecc97be967a19bf62a4394d1270170f5d4b6d9
pavania1 <adinapavani@gmail.com>
2023-12-29 23:17:13 +0530

    chore: change the landingpage

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -113,8 +113,9 @@ export const Landingpage = ({ children }: { children: React.ReactNode }) => {
                       Interchain Interface
                     </div>
                     <div className="text-white text-lg font-thin leading-normal">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididun.
+                      Resolute is an advanced spacecraft designed to travel
+                      through the multiverse, connecting Cosmos sovereign
+                      chains.
                     </div>
                   </div>
                   <div
@@ -136,10 +137,94 @@ export const Landingpage = ({ children }: { children: React.ReactNode }) => {
             </div>
 
             <Image
-              className="ml-auto"
-              src="/laptop.svg"
-              width={967}
-              height={581}
+              className="absolute right-[800px] top-[400px] image-animation"
+              src="/agoric-logo.png"
+              width={59}
+              height={59}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[900px] top-[190px] image-animation"
+              src="/akash-logo.png"
+              width={95}
+              height={95}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[800px] top-[600px] image-animation"
+              src="/tgrade-logo.png"
+              width={59}
+              height={59}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[900px] top-[700px] image-animation"
+              src="/logo5.png"
+              width={62}
+              height={62}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[200px] top-[100px] image-animation"
+              src="/desmos-logo.png"
+              width={75}
+              height={75}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[400px] top-[250px] image-animation"
+              src="/evmos-logo.png"
+              width={96}
+              height={96}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[100px] top-[200px] image-animation"
+              src="/osmosis-logo.png"
+              width={96}
+              height={96}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[300px] top-[400px] image-animation"
+              src="/stargaze-logo.png"
+              width={59}
+              height={59}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[450px] top-[500px] image-animation"
+              src="/regen-logo.png"
+              width={63}
+              height={63}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[250px] top-[600px] image-animation"
+              src="/dydx-logo.png"
+              width={106}
+              height={106}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[50px] top-[500px] image-animation"
+              src="/cosmos-logo.png"
+              width={94}
+              height={94}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[100px] top-[700px] image-animation"
+              src="/juno-logo.png"
+              width={93}
+              height={93}
+              alt="landing page image"
+            />
+            <Image
+              className="absolute right-[550px] top-[750px] image-animation"
+              src="/passage-logo.png"
+              width={97}
+              height={97}
               alt="landing page image"
             />
           </div>
