bb962c168dbe4d25cb55d4c3bb11efced2bde89d
hemanthghs <ghshemanth@gmail.com>
2024-01-04 11:35:29 +0530

    delete frontend folder

--- frontend/src/pages/group/GroupMembersInfo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GroupMembersInfo() {
-  return (
-    <div>GroupMembersInfo</div>
-  )
-}
-
-export default GroupMembersInfo
--- frontend/src/pages/group/common.ts
+++
@@ -1,6 +0,0 @@
-export const PER_PAGE = 10; 
-// From https://github.com/cosmos/cosmos-sdk/blob/release/v0.46.x/x/group/config.go#L17
-export const MAX_EXECUTION_PERIOD = 14;
-export const THRESHOLD = "threshold";
-export const PERCENTAGE = "percentage";
-export const DAYS = "Days";
