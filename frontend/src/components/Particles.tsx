9226626e290ca2e401061f7d1612dcc7ee2d65aa
saiteja <tejargukt123@gmail.com>
2024-01-04 11:28:48 +0530

    chore: removed unnecessary empty callbacl

--- frontend/src/components/Particles.tsx
+++ frontend/src/components/Particles.tsx
@@ -1,22 +1,17 @@
 import { useCallback } from 'react';
 import Particles from 'react-particles';
-import { Container, Engine } from 'tsparticles-engine';
+import { Engine } from 'tsparticles-engine';
 import { loadSlim } from 'tsparticles-slim';
 
 const CustomParticles = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadSlim(engine);
   }, []);
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {},
-    []
-  );
 
   return (
     <Particles
       id="tsparticles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         particles: {
           number: {
