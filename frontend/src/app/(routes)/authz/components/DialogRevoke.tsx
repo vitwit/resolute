9cef6630997231c64b3a2c10ac1fd8c66dd469e5
pavania1 <adinapavani@gmail.com>
2024-01-09 11:00:42 +0530

    chore: Revoke Dialog

--- frontend/src/app/(routes)/authz/components/DialogRevoke.tsx
+++ frontend/src/app/(routes)/authz/components/DialogRevoke.tsx
@@ -35,16 +35,30 @@ const DialogRevoke: React.FC<DialogRevokeProps> = (props) => {
             </div>
           </div>
          <div>
-            <div className='flex'>
+            <div className='flex h-[298px]'>
+              <div>
                 <Image
                 src="/revoke-image.png"
                 width={288}
                 height={238}
-                alt=""
+                alt="Revoke"
                 draggable={false}
                 />
+                </div>
+                <div className='flex flex-col gap-6  self-stretch px-10 py-0 mt-10'>
+                  <div className='text-white text-xl not-italic font-bold leading-[normal]'>Revoke Permission?</div>
+                  <div className='text-white text-sm not-italic font-light leading-[normal]'>Are you sure you want to delete this transaction ? This action cannot be undone</div>
+                  <div className='flex space-x-10 items-center'>
+                    <button
+                    className='create-grant-btn'>Revoke</button>
+                    <p className='text-white text-base not-italic font-medium leading-5 tracking-[0.64px] underline cursor-pointer'>Cancel</p>
+                  </div>
+                </div>
+               
             </div>
+            
          </div>
+         <div className='justify-end items-center gap-2.5 self-stretch pt-6 pb-0 px-6'></div>
         </div>
       </DialogContent>
     </Dialog>
