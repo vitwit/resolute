d1642240b631f9e24cd8c327d633664b476c3039
Teja2045 <106052623+Teja2045@users.noreply.github.com>
2024-01-09 11:02:06 +0530

    feat: integrated authz button (#1082)
    * feat: added authz slice
    
    * chore: fixed all state address mapping
    
    * chore: review changes
    
    * chore: fix error
    
    * feat: integrated authz fetch logic
    
    * chore: added chainID
    
    * feat: integrated authz button
    
    * feat: integrated button authz
    
    (cherry picked from commit 104c9fa10697a41b3713b6baf48e0085d3af4988)
    
    # Conflicts:
    #	frontend/src/custom-hooks/useAuthzGrants.ts
    #	frontend/src/store/features/authz/authzSlice.ts
    #	frontend/src/types/authz.d.ts

---
+++ frontend/src/store/features/authz/authzSlice.ts
@@ -0,0 +1,233 @@
+'use client';
+
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import authzService from './service';
+import { TxStatus } from '../../../types/enums';
+import { cloneDeep } from 'lodash';
+import { getAddressByPrefix } from '@/utils/address';
+
+interface ChainAuthz {
+  grantsToMe: Authorization[];
+  grantsByMe: Authorization[];
+  getGrantsToMeLoading: {
+    status: TxStatus;
+    errMsg: string;
+  };
+  getGrantsByMeLoading: {
+    status: TxStatus;
+    errMsg: string;
+  };
+
+  /*
+    this is mapping of address to list of authorizations (chain level)
+    example : {
+        "pasg1..." : ["stakeAuthorization...", "sendAuthorization..."]
+    }
+  */
+
+  GrantsToMeAddressMapping: Record<string, Authorization[]>;
+  GrantsByMeAddressMapping: Record<string, Authorization[]>;
+  tx: {
+    status: TxStatus;
+    errMsg: string;
+  };
+}
+
+const defaultState: ChainAuthz = {
+  grantsToMe: [],
+  grantsByMe: [],
+  getGrantsByMeLoading: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+  getGrantsToMeLoading: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+  GrantsByMeAddressMapping: {},
+  GrantsToMeAddressMapping: {},
+  tx: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+};
+
+interface AuthzState {
+  authzModeEnabled: boolean;
+  authzAddress: string;
+  chains: Record<string, ChainAuthz>;
+  getGrantsToMeLoading: number;
+  getGrantsByMeLoading: number;
+  /*
+    this is mapping of address to chain id to list of authorizations (inter chain level)
+    example : {
+        "cosmos1..." : {
+          "cosmoshub-4": ["stakeAuthorization...", "sendAuthorization..."]
+        }
+    }
+  */
+  AddressToChainAuthz: Record<string, Record<string, Authorization[]>>;
+}
+
+const initialState: AuthzState = {
+  authzModeEnabled: false,
+  authzAddress: '',
+  chains: {},
+  getGrantsByMeLoading: 0,
+  getGrantsToMeLoading: 0,
+  AddressToChainAuthz: {},
+};
+
+export const getGrantsToMe = createAsyncThunk(
+  'authz/grantsToMe',
+  async (data: GetGrantsInputs) => {
+    const response = await authzService.grantsToMe(
+      data.baseURL,
+      data.address,
+      data.pagination
+    );
+
+    return {
+      data: response.data,
+    };
+  }
+);
+
+export const getGrantsByMe = createAsyncThunk(
+  'authz/grantsByMe',
+  async (data: GetGrantsInputs) => {
+    const response = await authzService.grantsByMe(
+      data.baseURL,
+      data.address,
+      data.pagination
+    );
+    return {
+      data: response.data,
+    };
+  }
+);
+
+export const authzSlice = createSlice({
+  name: 'authz',
+  initialState,
+  reducers: {
+    enableAuthzMode: (state, action: PayloadAction<{ address: string }>) => {
+      state.authzModeEnabled = true;
+      state.authzAddress = action.payload.address;
+    },
+    exitAuthzMode: (state) => {
+      state.authzModeEnabled = false;
+      state.authzAddress = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGrantsToMe.pending, (state, action) => {
+        state.getGrantsToMeLoading++;
+        const chainID = action.meta.arg.chainID;
+        if (!state.chains[chainID])
+          state.chains[chainID] = cloneDeep(defaultState);
+        state.chains[chainID].getGrantsToMeLoading.status = TxStatus.PENDING;
+        state.chains[chainID].grantsToMe = [];
+        state.chains[chainID].GrantsToMeAddressMapping = {};
+        const allAddressToAuthz = state.AddressToChainAuthz;
+        const addresses = Object.keys(allAddressToAuthz);
+        addresses.forEach((address) => {
+          allAddressToAuthz[address][chainID] = [];
+        });
+
+        state.AddressToChainAuthz = allAddressToAuthz;
+      })
+      .addCase(getGrantsToMe.fulfilled, (state, action) => {
+        const chainID = action.meta.arg.chainID;
+        const allAddressToAuthz = state.AddressToChainAuthz;
+        const addresses = Object.keys(allAddressToAuthz);
+        addresses.forEach((address) => {
+          allAddressToAuthz[address][chainID] = [];
+        });
+
+        state.AddressToChainAuthz = allAddressToAuthz;
+
+        state.getGrantsToMeLoading--;
+
+        const grants = action.payload.data.grants;
+        state.chains[chainID].grantsToMe = grants;
+        const addressMapping: Record<string, Authorization[]> = {};
+        const allChainsAddressToGrants = state.AddressToChainAuthz;
+
+        grants.forEach((grant) => {
+          const granter = grant.granter;
+          const cosmosAddress = getAddressByPrefix(granter, 'cosmos');
+          if (!addressMapping[granter]) addressMapping[granter] = [];
+          if (!allChainsAddressToGrants[cosmosAddress])
+            allChainsAddressToGrants[cosmosAddress] = {};
+          if (!allChainsAddressToGrants[cosmosAddress][chainID])
+            allChainsAddressToGrants[cosmosAddress][chainID] = [];
+          allChainsAddressToGrants[cosmosAddress][chainID] = [
+            ...allChainsAddressToGrants[cosmosAddress][chainID],
+            grant,
+          ];
+          addressMapping[granter] = [...addressMapping[granter], grant];
+        });
+        state.AddressToChainAuthz = allChainsAddressToGrants;
+        state.chains[chainID].GrantsToMeAddressMapping = addressMapping;
+        state.chains[chainID].getGrantsToMeLoading = {
+          status: TxStatus.IDLE,
+          errMsg: '',
+        };
+      })
+      .addCase(getGrantsToMe.rejected, (state, action) => {
+        state.getGrantsToMeLoading--;
+        const chainID = action.meta.arg.chainID;
+
+        state.chains[chainID].getGrantsToMeLoading = {
+          status: TxStatus.REJECTED,
+          errMsg:
+            action.error.message ||
+            'An error occurred while fetching authz grants to me',
+        };
+      });
+    builder
+      .addCase(getGrantsByMe.pending, (state, action) => {
+        state.getGrantsByMeLoading++;
+        const chainID = action.meta.arg.chainID;
+        if (!state.chains[chainID])
+          state.chains[chainID] = cloneDeep(defaultState);
+        state.chains[chainID].getGrantsByMeLoading.status = TxStatus.PENDING;
+        state.chains[chainID].grantsByMe = [];
+        state.chains[chainID].GrantsByMeAddressMapping = {};
+      })
+      .addCase(getGrantsByMe.fulfilled, (state, action) => {
+        state.getGrantsByMeLoading--;
+        const chainID = action.meta.arg.chainID;
+        const grants = action.payload.data.grants;
+        state.chains[chainID].grantsByMe = grants;
+        const addressMapping: Record<string, Authorization[]> = {};
+        grants.forEach((grant) => {
+          const granter = grant.granter;
+          if (!addressMapping[granter]) addressMapping[granter] = [];
+          addressMapping[granter] = [...addressMapping[granter], grant];
+        });
+        state.chains[chainID].GrantsByMeAddressMapping = addressMapping;
+        state.chains[chainID].getGrantsByMeLoading = {
+          status: TxStatus.IDLE,
+          errMsg: '',
+        };
+      })
+      .addCase(getGrantsByMe.rejected, (state, action) => {
+        state.getGrantsByMeLoading--;
+        const chainID = action.meta.arg.chainID;
+
+        state.chains[chainID].getGrantsByMeLoading = {
+          status: TxStatus.REJECTED,
+          errMsg:
+            action.error.message ||
+            'An error occurred while fetching authz grants by me',
+        };
+      });
+  },
+});
+
+export const { enableAuthzMode, exitAuthzMode } = authzSlice.actions;
+
+export default authzSlice.reducer;
