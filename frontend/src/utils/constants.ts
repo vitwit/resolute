263b119634cec3b79370290b9150d7aeda477729
Hemanth Sai <ghshemanth@gmail.com>
2024-01-23 18:49:32 +0530

    chore: Update add network config (#1126)
    * chore: Update add network config
    
    * update network config template file url
    
    ---------
    
    Co-authored-by: chary <57086313+charymalloju@users.noreply.github.com>

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -16,7 +16,7 @@ export const SUPPORTED_WALLETS = [
 export const USD_CURRENCY = 'usd';
 export const GAS_FEE = 860000;
 export const ADD_NETWORK_TEMPLATE_URL =
-  'https://raw.githubusercontent.com/vitwit/resolute/b5d184c8da894b2fea0ed40e56a599a1d813c422/frontend/public/add-network-template.json';
+  'https://raw.githubusercontent.com/vitwit/resolute/88d2fe5f62b300e90b122d10a257f9acef89643b/frontend/public/add-network-template.json';
 export const PROPOSAL_STATUS_VOTING_PERIOD = 'PROPOSAL_STATUS_VOTING_PERIOD';
 export const COSMOS_CHAIN_ID = 'cosmoshub-4';
 export const OFFCHAIN_VERIFICATION_MESSAGE =
