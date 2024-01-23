95f2cde6cf0a39323bdea2f31af3cd6067732973
hemanthghs <ghshemanth@gmail.com>
2024-01-23 21:53:06 +0530

    feat: add feegrant slice

---
+++ frontend/src/store/features/feegrant/feegrantService.ts
@@ -0,0 +1,44 @@
+import Axios, { AxiosResponse } from 'axios';
+import { convertPaginationToParams, cleanURL } from '@/utils/util';
+import { axiosGetRequestWrapper } from '@/utils/RequestWrapper';
+import { MAX_TRY_END_POINTS } from '@/utils/constants';
+
+const grantToMeURL = '/cosmos/feegrant/v1beta1/allowances/';
+const grantByMeURL = '/cosmos/feegrant/v1beta1/issued/';
+
+const fetchGrantsToMe = (
+  baseURLs: string[],
+  grantee: string,
+  pagination?: KeyLimitPagination
+): Promise<AxiosResponse<GetFeegrantsResponse>> => {
+  let endPoint = `${grantToMeURL}${grantee}`;
+
+  const parsed = convertPaginationToParams(pagination);
+  if (parsed !== '') {
+    endPoint += `?${parsed}`;
+  }
+
+  return axiosGetRequestWrapper(baseURLs, endPoint, MAX_TRY_END_POINTS);
+};
+
+const fetchGrantsByMe = (
+  baseURLs: string[],
+  grantee: string,
+  pagination?: KeyLimitPagination
+): Promise<AxiosResponse<GetFeegrantsResponse>> => {
+  let endPoint = `${grantByMeURL}${grantee}`;
+
+  const parsed = convertPaginationToParams(pagination);
+  if (parsed !== '') {
+    endPoint += `?${parsed}`;
+  }
+
+  return axiosGetRequestWrapper(baseURLs, endPoint, MAX_TRY_END_POINTS);
+};
+
+const result = {
+  grantsByMe: fetchGrantsByMe,
+  grantsToMe: fetchGrantsToMe,
+};
+
+export default result;
---
+++ frontend/src/store/features/feegrant/feegrantSlice.ts
@@ -0,0 +1,207 @@
+import { TxStatus } from '@/types/enums';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import feegrantService from './feegrantService';
+import { cloneDeep } from 'lodash';
+import { getAddressByPrefix } from '@/utils/address';
+
+interface ChainAllowance {
+  grantsToMe: Allowance[];
+  grantsByMe: Allowance[];
+  getGrantsToMeLoading: {
+    status: TxStatus;
+    errMsg: string;
+  };
+  getGrantsByMeLoading: {
+    status: TxStatus;
+    errMsg: string;
+  };
+  grantsToMeAddressMapping: Record<string, Allowance[]>;
+  grantsByMeAddressMapping: Record<string, Allowance[]>;
+  tx: {
+    status: TxStatus;
+    errMsg: string;
+  };
+}
+
+const defaultState: ChainAllowance = {
+  grantsToMe: [],
+  grantsByMe: [],
+  getGrantsByMeLoading: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+  getGrantsToMeLoading: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+  grantsByMeAddressMapping: {},
+  grantsToMeAddressMapping: {},
+  tx: {
+    status: TxStatus.INIT,
+    errMsg: '',
+  },
+};
+
+interface FeegrantState {
+  feegrantModeEnabled: boolean;
+  feegrantAddress: string;
+  chains: Record<string, ChainAllowance>;
+  getGrantsToMeLoading: number;
+  getGrantsByMeLoading: number;
+  addressToChainFeegrant: Record<string, Record<string, Allowance[]>>;
+}
+
+const initialState: FeegrantState = {
+  feegrantModeEnabled: false,
+  feegrantAddress: '',
+  chains: {},
+  getGrantsByMeLoading: 0,
+  getGrantsToMeLoading: 0,
+  addressToChainFeegrant: {},
+};
+
+export const getGrantsToMe = createAsyncThunk(
+  'feegrant/grantsToMe',
+  async (data: GetFeegrantsInputs) => {
+    const response = await feegrantService.grantsToMe(
+      data.baseURLs,
+      data.address,
+      data.pagination
+    );
+
+    return {
+      data: response.data,
+    };
+  }
+);
+
+export const getGrantsByMe = createAsyncThunk(
+  'feegrant/grantsByMe',
+  async (data: GetFeegrantsInputs) => {
+    const response = await feegrantService.grantsByMe(
+      data.baseURLs,
+      data.address,
+      data.pagination
+    );
+
+    return {
+      data: response.data,
+    };
+  }
+);
+
+export const feegrantSlice = createSlice({
+  name: 'feegrant',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGrantsToMe.pending, (state, action) => {
+        state.getGrantsToMeLoading++;
+        const chainID = action.meta.arg.chainID;
+        if (!state.chains[chainID])
+          state.chains[chainID] = cloneDeep(defaultState);
+        state.chains[chainID].getGrantsToMeLoading.status = TxStatus.PENDING;
+        state.chains[chainID].grantsToMe = [];
+        state.chains[chainID].grantsToMeAddressMapping = {};
+        const allAddressToFeegrant = state.addressToChainFeegrant;
+        const addresses = Object.keys(allAddressToFeegrant);
+        addresses.forEach((address) => {
+          allAddressToFeegrant[address][chainID] = [];
+        });
+
+        state.addressToChainFeegrant = allAddressToFeegrant;
+      })
+      .addCase(getGrantsToMe.fulfilled, (state, action) => {
+        const chainID = action.meta.arg.chainID;
+        const allAddressToFeegrant = state.addressToChainFeegrant;
+        const addresses = Object.keys(allAddressToFeegrant);
+        addresses.forEach((address) => {
+          allAddressToFeegrant[address][chainID] = [];
+        });
+
+        state.addressToChainFeegrant = allAddressToFeegrant;
+
+        state.getGrantsToMeLoading--;
+
+        const grants = action.payload.data.allowances;
+        state.chains[chainID].grantsToMe = grants;
+        const addressMapping: Record<string, Allowance[]> = {};
+        const allChainsAddressToGrants = state.addressToChainFeegrant;
+
+        grants.forEach((grant: Allowance) => {
+          const granter = grant.granter;
+          const cosmosAddress = getAddressByPrefix(granter, 'cosmos');
+          if (!addressMapping[granter]) addressMapping[granter] = [];
+          if (!allChainsAddressToGrants[cosmosAddress])
+            allChainsAddressToGrants[cosmosAddress] = {};
+          if (!allChainsAddressToGrants[cosmosAddress][chainID])
+            allChainsAddressToGrants[cosmosAddress][chainID] = [];
+          allChainsAddressToGrants[cosmosAddress][chainID] = [
+            ...allChainsAddressToGrants[cosmosAddress][chainID],
+            grant,
+          ];
+          addressMapping[granter] = [...addressMapping[granter], grant];
+        });
+        state.addressToChainFeegrant = allChainsAddressToGrants;
+        state.chains[chainID].grantsToMeAddressMapping = addressMapping;
+        state.chains[chainID].getGrantsToMeLoading = {
+          status: TxStatus.IDLE,
+          errMsg: '',
+        };
+      })
+      .addCase(getGrantsToMe.rejected, (state, action) => {
+        state.getGrantsToMeLoading--;
+        const chainID = action.meta.arg.chainID;
+
+        state.chains[chainID].getGrantsToMeLoading = {
+          status: TxStatus.REJECTED,
+          errMsg:
+            action.error.message ||
+            'An error occurred while fetching feegrants to me',
+        };
+      });
+    builder
+      .addCase(getGrantsByMe.pending, (state, action) => {
+        state.getGrantsByMeLoading++;
+        const chainID = action.meta.arg.chainID;
+        if (!state.chains[chainID])
+          state.chains[chainID] = cloneDeep(defaultState);
+        state.chains[chainID].getGrantsByMeLoading.status = TxStatus.PENDING;
+        state.chains[chainID].grantsByMe = [];
+        state.chains[chainID].grantsByMeAddressMapping = {};
+      })
+      .addCase(getGrantsByMe.fulfilled, (state, action) => {
+        state.getGrantsByMeLoading--;
+        const chainID = action.meta.arg.chainID;
+        const grants = action.payload.data.allowances;
+        state.chains[chainID].grantsByMe = grants;
+        const addressMapping: Record<string, Allowance[]> = {};
+        grants.forEach((grant: Allowance) => {
+          const granter = grant.grantee;
+          if (!addressMapping[granter]) addressMapping[granter] = [];
+          addressMapping[granter] = [...addressMapping[granter], grant];
+        });
+        state.chains[chainID].grantsByMeAddressMapping = addressMapping;
+        state.chains[chainID].getGrantsByMeLoading = {
+          status: TxStatus.IDLE,
+          errMsg: '',
+        };
+      })
+      .addCase(getGrantsByMe.rejected, (state, action) => {
+        state.getGrantsByMeLoading--;
+        const chainID = action.meta.arg.chainID;
+
+        state.chains[chainID].getGrantsByMeLoading = {
+          status: TxStatus.REJECTED,
+          errMsg:
+            action.error.message ||
+            'An error occurred while fetching authz grants by me',
+        };
+      });
+  },
+});
+
+export const {} = feegrantSlice.actions;
+
+export default feegrantSlice.reducer;
