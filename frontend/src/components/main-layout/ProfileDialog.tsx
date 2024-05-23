3d920ab49d4cf12cc871382c29ead6c0aec8a09f
hemanthghs <ghshemanth@gmail.com>
2024-05-23 15:18:30 +0530

    feat: implement connect wallet

---
+++ frontend/src/components/main-layout/ProfileDialog.tsx
@@ -0,0 +1,77 @@
+import { useAppSelector } from '@/custom-hooks/StateHooks';
+import { getConnectWalletLogo } from '@/utils/util';
+import { Dialog, DialogContent, Slide, SlideProps } from '@mui/material';
+import Image from 'next/image';
+import React, { forwardRef } from 'react';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const Transition = forwardRef(function Transition(
+  props: SlideProps & { children: React.ReactElement<any, any> },
+  ref: React.Ref<any>
+) {
+  return <Slide direction="left" ref={ref} {...props} timeout={500} />;
+});
+
+const ProfileDialog = ({
+  onClose,
+  open,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) => {
+  const connnectedWalletLogo = getConnectWalletLogo();
+  const walletUserName = useAppSelector((state) => state.wallet.name);
+
+  return (
+    <Dialog
+      open={open}
+      maxWidth="lg"
+      TransitionComponent={Transition}
+      sx={{
+        '& .MuiDialog-paper': {
+          position: 'absolute',
+          right: '30px',
+          left: 'auto',
+          height: 'calc(100% - 60px)',
+          margin: 0,
+          color: 'white',
+        },
+      }}
+      PaperProps={{
+        sx: {
+          borderRadius: '16px',
+          background: '#1c1c1d',
+        },
+      }}
+      onClose={onClose}
+    >
+      <DialogContent sx={{ padding: 0 }}>
+        <div className="profile-section">
+          <div className="space-y-6">
+            <div className="space-y-2">
+              <div className="text-[20px] font-bold leading-[27px]">
+                Profile
+              </div>
+              <div className="secondary-text">
+                Connect your wallet now to access all the modules on resolute
+              </div>
+              <div className="divider-line"></div>
+            </div>
+            <div className="flex flex-col items-center gap-2 px-6 py-[10px]">
+              <Image src={connnectedWalletLogo} height={40} width={40} alt="" />
+              <div className="font-medium">{walletUserName}</div>
+            </div>
+            <div>
+              <button className="primary-btn w-full">Logout</button>
+            </div>
+            <button onClick={onClose} className="secondary-btn w-full">
+              Close Tab
+            </button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProfileDialog;
--- frontend/src/components/main-layout/SideMenu.tsx
+++ frontend/src/components/main-layout/SideMenu.tsx
@@ -9,7 +9,7 @@ const SideMenu = () => {
   // TODO: Add more option for Transfers and Settings MenuItem
   return (
     <div className="scrollable-content w-full">
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-2">
         {SIDEBAR_MENU_OPTIONS.map((item) => (
           <MenuItem key={item.name} itemData={item} pathName={pathName} />
         ))}
