60acab4cc2b5e3a65d51ebb8963dfa43be935f84
pavania1 <adinapavani@gmail.com>
2024-05-28 16:52:20 +0530

    chore: Multisig

--- frontend/src/app/(routes)/multisig/page.tsx
+++ frontend/src/app/(routes)/multisig/page.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Multisig from './Multisig';
-import MultisigWalletConnection from './components/MultisigWalletConnection';
-import MultisigOwnership from './components/MultisigOwnership';
-import MultisigDashboard from './components/MultisigDashboard';
-import SingleMultisig from './components/SingleMultisig';
 
 const page = () => {
   return (
