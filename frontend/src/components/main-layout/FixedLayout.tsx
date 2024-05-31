1a9b41045c2c73b803b8d52615b5299d54fbceec
pavania1 <117721819+pavania1@users.noreply.github.com>
2024-05-31 15:07:59 +0530

    Pavania1/stakingpopups (#1251)
    * chore: DelegatePopup
    
    * chore: Undelegate Popup
    
    * feat: add transaction status popup (#1250)
    
    feat: add txn status popup
    
    * chore: ReDeleagtePopup
    
    * chore: Staking Popups
    
    ---------
    
    Co-authored-by: Hemanth Sai <ghshemanth@gmail.com>

--- frontend/src/components/main-layout/FixedLayout.tsx
+++ frontend/src/components/main-layout/FixedLayout.tsx
@@ -23,6 +23,7 @@ import {
 } from '@/utils/localStorage';
 import { setAllNetworksInfo } from '@/store/features/common/commonSlice';
 import useShortCuts from '@/custom-hooks/useShortCuts';
+import TransactionStatusPopup from '../txn-status-popups/TransactionStatusPopup';
 
 const FixedLayout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
@@ -97,6 +98,7 @@ const FixedLayout = ({ children }: { children: React.ReactNode }) => {
           <section className="dynamic-section">{children}</section>
         </div>
       </main>
+      <TransactionStatusPopup />
     </div>
   );
 };
