5a039f6f1090b5fc778edcc8e0c925b4bbe0fc6d
pavania1 <adinapavani@gmail.com>
2024-06-03 15:27:10 +0530

    chore: Ui changes in staking

--- frontend/src/app/(routes)/staking/component/StakingDelegations.tsx
+++ frontend/src/app/(routes)/staking/component/StakingDelegations.tsx
@@ -1,108 +1,129 @@
-
-
 import useStaking from '@/custom-hooks/useStaking';
 import { get } from 'lodash';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 import ValidatorName from './ValidatorName';
 import useValidator from '@/custom-hooks/useValidator';
 import { Chains } from '@/store/features/staking/stakeSlice';
 
-function StakingDelegations({  delegations }: { delegations: Chains }) {
-    const staking = useStaking()
+function StakingDelegations({ delegations }: { delegations: Chains }) {
+  const staking = useStaking();
 
-    const validator = useValidator()
+  const validator = useValidator();
 
-    const getCommisionRate = (valAddress: string, chainID: string) => {
-        const v = validator.getValidatorDetails(valAddress, chainID)
-        return (Number(get(v, 'commission.commission_rates.rate', 0)) * 100)
-    }
+  const getCommisionRate = (valAddress: string, chainID: string) => {
+    const v = validator.getValidatorDetails(valAddress, chainID);
+    return Number(get(v, 'commission.commission_rates.rate', 0)) * 100;
+  };
 
-    const getChainTotalRewards = (chainID: string) => {
-        return staking.chainTotalRewards(chainID)
-    }
+  const getChainTotalRewards = (chainID: string) => {
+    return staking.chainTotalRewards(chainID);
+  };
 
-    return (
-        <div className="flex flex-col w-full gap-10">
-            <div className="space-y-2 items-start">
-                <div className="text-white text-[28px] not-italic font-bold leading-[normal]">
-                    Delegations
-                </div>
-                <div className="text-[rgba(255,255,255,0.50)] text-sm not-italic font-extralight leading-8">
-                    Connect your wallet now to access all the modules on resolute{' '}
-                </div>
-                <div className="horizontal-line"></div>
-            </div>
-
-            {
-                Object.entries(delegations).map(([key, value], index) => {
-                    return get(value, 'delegations.delegations.delegation_responses.length') &&
-                        <div key={index} className="flex flex-col items-start gap-4 self-stretch px-6 py-0">
-                            <div className="flex justify-between w-full">
-                                <div className="flex space-x-4">
-                                    <div className="space-x-2 flex">
-                                        <Image
-                                            src={staking.chainLogo(key)}
-                                            width={38}
-                                            height={30}
-                                            alt="akash-logo"
-                                        />
-                                        <p className="text-white text-base not-italic font-normal leading-8">
-                                            {key}
-                                        </p>
-                                    </div>
-                                    <div className="red-button text-white text-[10px] not-italic font-light leading-6">
-                                        Total staked : &nbsp;
-                                        {staking.getAmountWithDecimal(Number(get(value, 'delegations.totalStaked', 0)), key)}
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <button className="primary-btn cursor-pointer">Claim
-                                        &nbsp;
-                                        {getChainTotalRewards(key)}
-                                        {/* 12.4.5 AKT */}
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-1 w-full gap-4">
-                                {get(value, 'delegations.delegations.delegation_responses', []).map((data, dataid) => (
-                                    <div key={dataid} className="delegations-card w-full">
-                                        <div className="flex items-center justify-between self-stretch">
-                                            <div className="flex flex-col items-start gap-2">
-                                                <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                    Validator Name
-                                                </p>
-                                                <ValidatorName valoperAddress={get(data, 'delegation.validator_address')} chainID={key} />
-                                            </div>
-                                            <div className="flex flex-col items-start gap-2">
-                                                <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                    Staked Amount
-                                                </p>
-                                                <p className="text-white text-sm not-italic font-normal leading-[normal]">
-                                                    {staking.getAmountWithDecimal(Number(get(data, 'balance.amount')), key)}
-                                                </p>
-                                            </div>
-                                            <div className="flex flex-col items-start gap-2">
-                                                <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                    Commission
-                                                </p>
-                                                <p className="text-white text-sm not-italic font-normal leading-[normal]">
-                                                    {
-                                                        getCommisionRate(get(data, 'delegation.validator_address'), key)
-                                                    } %
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                })
-            }
-
-
+  return (
+    <div className="flex flex-col w-full gap-10">
+      <div className="space-y-2 items-start">
+        <div className="text-h2">Delegations</div>
+        <div className="secondary-text">
+          Connect your wallet now to access all the modules on resolute{' '}
         </div>
-    )
+        <div className="horizontal-line"></div>
+      </div>
+
+      {Object.entries(delegations).map(([key, value], index) => {
+        return (
+          get(value, 'delegations.delegations.delegation_responses.length') && (
+            <div key={index} className="px-6 py-0">
+              <div className="flex justify-between w-full mb-4">
+                <div className="flex space-x-4">
+                  <div className="space-x-2 flex justify-center items-center">
+                    <Image
+                      src={staking.chainLogo(key)}
+                      width={32}
+                      height={32}
+                      className="h-8 w-8"
+                      alt="akash-logo"
+                    />
+                    <p className="text-white text-base font-normal leading-8 flex justify-center items-center">
+                      {key}
+                    </p>
+                  </div>
+                  <div className="staked-amount-red-badge text-white text-[10px] font-light leading-6">
+                    Total staked : &nbsp;
+                    {staking.getAmountWithDecimal(
+                      Number(get(value, 'delegations.totalStaked', 0)),
+                      key
+                    )}
+                  </div>
+                </div>
+                <div className="">
+                  <button className="primary-btn">
+                    Claim &nbsp;
+                    {getChainTotalRewards(key)}
+                    {/* 12.4.5 AKT */}
+                    <p className="ml-2 text-small-light">Rewards</p>
+                  </button>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 w-full gap-4">
+                {get(
+                  value,
+                  'delegations.delegations.delegation_responses',
+                  []
+                ).map((data, dataid) => (
+                  <div key={dataid} className="delegations-card w-full">
+                    <div className="flex items-center justify-between w-full gap-10">
+                      <div className="flex flex-col items-start gap-2 w-1/3">
+                        <p className="text-small">Validator Name</p>
+                        <ValidatorName
+                          valoperAddress={get(
+                            data,
+                            'delegation.validator_address'
+                          )}
+                          chainID={key}
+                        />
+                      </div>
+                      <div className="flex flex-col items-start gap-2 w-1/4">
+                        <p className="text-small">Staked Amount</p>
+                        <p className="text-b1">
+                          {staking.getAmountWithDecimal(
+                            Number(get(data, 'balance.amount')),
+                            key
+                          )}
+                        </p>
+                      </div>
+                      <div className="flex flex-col items-start gap-2 w-1/4">
+                        <p className="text-small">Rewards</p>
+                        <p className="text-b1">0.9876 AKT</p>
+                      </div>
+                      <div className="flex flex-col items-start gap-2 w-1/4">
+                        <p className="text-small">Commission</p>
+                        <p className="text-b1">
+                          {getCommisionRate(
+                            get(data, 'delegation.validator_address'),
+                            key
+                          )}{' '}
+                          %
+                        </p>
+                      </div>
+                      <div className="flex justify-end">
+                        <Image
+                          src="/more.svg"
+                          width={24}
+                          height={24}
+                          alt="More-Icon"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )
+        );
+      })}
+    </div>
+  );
 }
 
-export default StakingDelegations
+export default StakingDelegations;
--- frontend/src/app/(routes)/staking/component/StakingUnDelegations.tsx
+++ frontend/src/app/(routes)/staking/component/StakingUnDelegations.tsx
@@ -1,83 +1,74 @@
 import useStaking from '@/custom-hooks/useStaking';
 import { get } from 'lodash';
 // import Image from 'next/image'
-import React from 'react'
+import React from 'react';
 import ValidatorName from './ValidatorName';
 import { getTimeDifferenceToFutureDate } from '@/utils/dataTime';
 import { Chains } from '@/store/features/staking/stakeSlice';
+import '../staking.css';
 
-function StakingUnDelegations({undelegations }: {  undelegations: Chains }) {
+function StakingUnDelegations({ undelegations }: { undelegations: Chains }) {
+  const staking = useStaking();
 
-    const staking = useStaking()
-
-    return (
-        <div className="flex flex-col w-full gap-10">
-            <div className="space-y-2 items-start">
-                <div className="text-white text-[28px] not-italic font-bold leading-[normal]">
-                    Unbonding
-                </div>
-                <div className="text-[rgba(255,255,255,0.50)] text-sm not-italic font-extralight leading-8">
-                    Connect your wallet now to access all the modules on resolute{' '}
-                </div>
-                <div className="horizontal-line"></div>
-            </div>
-            <div className="grid grid-cols-3 gap-10 px-6 py-0">
-                {
-                    Object.entries(undelegations).map(([key, value]) => {
-                        return get(value, 'unbonding.unbonding.unbonding_responses', []).map((ud, index) => {
-                            return get(ud, 'entries', []).map((e) => {
-                                return <div key={index} className="cards">
-                                    <div className="flex items-start justify-between self-stretch">
-                                        <div className="flex space-x-2">
-                                            <ValidatorName valoperAddress={get(ud, 'validator_address', '')} chainID={key} />
-                                        </div>
-                                        <div className="">
-                                            <button className="custom-btn text-white text-center text-sm not-italic font-light leading-[normal]">
-                                                Cancel
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div className="flex justify-between self-stretch">
-                                        <div className="flex flex-col items-start gap-2">
-                                            <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                Network
-                                            </p>
-                                            <p className="text-white text-sm not-italic font-normal leading-[normal]">
-                                                {key}
-                                            </p>
-                                        </div>
-                                        <div className="flex flex-col items-start gap-2">
-                                            <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                Avail Days
-                                            </p>
-                                            <p className="text-white text-sm not-italic font-normal leading-[normal]">
-                                                {
-                                                    getTimeDifferenceToFutureDate(get(e, 'completion_time', ''))
-                                                }
-                                            </p>
-                                        </div>
-                                        <div className="flex flex-col items-start gap-2">
-                                            <p className="text-[rgba(255,255,255,0.50)] text-xs not-italic font-normal leading-[normal]">
-                                                Amount
-                                            </p>
-                                            <p className="text-white text-sm not-italic font-normal leading-[normal]">
-                                                {staking.getAmountWithDecimal(Number(get(e, 'balance')), key)}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            }
-                            )
-
-                        })
-                    })
-                }
-
-
-
-            </div>
+  return (
+    <div className="flex flex-col w-full gap-10">
+      <div className="space-y-2 items-start">
+        <div className="text-h2">Unbonding</div>
+        <div className="secondary-text">
+          Connect your wallet now to access all the modules on resolute{' '}
         </div>
-    )
+        <div className="horizontal-line"></div>
+      </div>
+      <div className="grid grid-cols-3 gap-10 px-6 py-0">
+        {Object.entries(undelegations).map(([key, value]) => {
+          return get(value, 'unbonding.unbonding.unbonding_responses', []).map(
+            (ud, index) => {
+              return get(ud, 'entries', []).map((e) => {
+                return (
+                  <div key={index} className="unBonding-card">
+                    <div className="flex justify-between w-full">
+                      <div className="flex space-x-2 justify-center items-center">
+                        <ValidatorName
+                          valoperAddress={get(ud, 'validator_address', '')}
+                          chainID={key}
+                        />
+                      </div>
+                      <div className="">
+                        <button className="primary-btn">Cancel</button>
+                      </div>
+                    </div>
+                    <div className="flex justify-between w-full">
+                      <div className="flex flex-col items-start gap-2">
+                        <p className="text-small">Network</p>
+                        <p className="text-b1">{key}</p>
+                      </div>
+                      <div className="flex flex-col items-start gap-2">
+                        <p className="text-small">Avail Days</p>
+                        <p className="text-b1">
+                          {getTimeDifferenceToFutureDate(
+                            get(e, 'completion_time', '')
+                          )}
+                        </p>
+                      </div>
+                      <div className="flex flex-col items-start gap-2">
+                        <p className="text-small">Amount</p>
+                        <p className="text-b1">
+                          {staking.getAmountWithDecimal(
+                            Number(get(e, 'balance')),
+                            key
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              });
+            }
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default StakingUnDelegations
+export default StakingUnDelegations;
