dc5ac0e6c12bc33fd98f332403bb7720dda386e0
charymalloju <chary@vitwit.com>
2024-06-11 23:32:03 +0530

    fix no data illustration

--- frontend/src/components/illustrations/withConnectionIllustration.tsx
+++ frontend/src/components/illustrations/withConnectionIllustration.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 const WithConnectionIllustration = ({message}: {message: string}) => {
   return (
     <div className="flex flex-col justify-center  gap-10 w-full px-10 py-20">
-      <div className="flex flex-col items-start space-y-1">
+      {/* <div className="flex flex-col items-start space-y-1">
         <div className="text-h1">Staking</div>
         <div className="text-b1-light">
           Connect your wallet now to access all the modules on resolute{' '}
         </div>
         <div className="divider-line"></div>
-      </div>
+      </div> */}
 
       <div className="flex flex-col justify-center items-center gap-10">
         <div className="flex flex-col gap-6">
