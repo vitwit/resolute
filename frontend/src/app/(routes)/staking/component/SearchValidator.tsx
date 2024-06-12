255c43734df821c4002a4017f305617a5989fd13
pavania1 <adinapavani@gmail.com>
2024-06-12 16:50:58 +0530

    chore: fixed lint issue

--- frontend/src/app/(routes)/staking/component/SearchValidator.tsx
+++ frontend/src/app/(routes)/staking/component/SearchValidator.tsx
@@ -1,30 +1,3 @@
-// import Image from 'next/image';
-// import React from 'react';
-
-// const SearchNetworkInput = ({
-//   searchQuery,
-//   handleSearchQueryChange,
-// }: {
-//   searchQuery: string;
-//   handleSearchQueryChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-// }) => {
-//   return (
-//     <div className="flex gap-2 items-center flex-1">
-//       <Image src="/icons/search-icon.svg" height={24} width={24} alt="" />
-//       <input
-//         type="text"
-//         placeholder="Search Network"
-//         value={searchQuery}
-//         onChange={handleSearchQueryChange}
-//         className="search-network-input"
-//         autoFocus={true}
-//       />
-//     </div>
-//   );
-// };
-
-// export default SearchNetworkInput;
-
 import React from 'react';
 import Image from 'next/image';
 
