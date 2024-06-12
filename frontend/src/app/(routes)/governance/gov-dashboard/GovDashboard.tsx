500a66553ab9a6cdcfa5ac093f3bda11558e014a
charymalloju <chary@vitwit.com>
2024-06-12 16:57:18 +0530

    fix filter in gov

--- frontend/src/app/(routes)/governance/gov-dashboard/GovDashboard.tsx
+++ frontend/src/app/(routes)/governance/gov-dashboard/GovDashboard.tsx
@@ -18,6 +18,7 @@ const GovDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
   const { getProposals } = useGetProposals();
   const [showAll, setShowAll] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
+  const [filterDays, setFilterDays] = useState(0);
   const propsData = getProposals({ chainIDs, showAll: showAll });
   const proposalsData = getProposals({ chainIDs, showAll: showAll });
   const [filteredProposals, setFilteredProposals] = useState<ProposalsData[]>(
@@ -54,13 +55,43 @@ const GovDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
 
     debounceTimeout.current = window.setTimeout(() => {
       const filtered = propsData.filter(
-        (proposal) =>
-          proposal.proposalInfo.proposalId.includes(query) ||
+        (proposal) => {
+          return  proposal.proposalInfo.proposalId.includes(query) ||
           proposal.proposalInfo.proposalTitle
             .toLowerCase()
             .includes(query.toLowerCase()) ||
           proposal.chainName.toLowerCase().includes(query.toLowerCase())
+        }
+         
       );
+      setFilteredProposals(filtered);
+    }, 500);
+  };
+
+  const handleFiltersChange = (days: number) => {  
+    setFilterDays(days)
+
+    if (debounceTimeout.current) {
+      clearTimeout(debounceTimeout.current);
+    }
+
+    debounceTimeout.current = window.setTimeout(() => {
+      const filtered = propsData.filter(
+        (proposal) => {
+          if (!days) {
+            return true
+          }
+
+          const daysNo = proposal.proposalInfo.endTime.match(/\d+/) || 0;
+          if (daysNo && daysNo.length) {
+            if (Number(daysNo[0])<= days){
+              return true
+            } 
+          }
+        }
+      );
+
+      console.log({filtered})
       setFilteredProposals(filtered);
     }, 500);
   };
@@ -78,11 +109,12 @@ const GovDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
           handleSearchQueryChange={handleSearchQueryChange}
           searchQuery={searchQuery}
           handleShowAllProposals={handleShowAllProposals}
+          handleFiltersChange={handleFiltersChange}
         />
       </div>
       <div className="flex gap-6 w-full flex-1 h-full overflow-y-scroll py-6">
         <div className="flex flex-col w-full gap-6 py-0 pb-6 flex-1 overflow-y-scroll">
-          {searchQuery?.length ? (
+          {searchQuery?.length || filterDays ? (
             <ProposalsList
               proposals={filteredProposals}
               handleViewProposal={handleViewProposal}
@@ -115,20 +147,37 @@ const QuickFilters = ({
   handleSearchQueryChange,
   searchQuery,
   handleShowAllProposals,
+  handleFiltersChange,
 }: {
   searchQuery: string;
   handleSearchQueryChange: HandleInputChangeEvent;
   handleShowAllProposals: HandleInputChangeEvent;
+  handleFiltersChange: (n: number) => void;
 }) => {
   // TODO: Add quick filters (Voting ends in 1 day & Deposit ends in 1 day)
   return (
-    <div className="h-[56px] flex flex-col items-end">
-      <SearchProposalInput
-        handleSearchQueryChange={handleSearchQueryChange}
-        searchQuery={searchQuery}
-        handleShowAllProposals={handleShowAllProposals}
-      />
+    <div>
+      <div className='flex py-2 gap-4'>
+      <button onClick={()=>handleFiltersChange(0)} className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'>
+          All
+        </button>
+        <button onClick={()=>handleFiltersChange(2)} className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'>
+          Voting ends in 2 days
+        </button>
+        <button onClick={()=>handleFiltersChange(1)} className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'>
+          Voting ends in 1 days
+        </button>
+      </div>
+
+      <div className="h-[56px] flex flex-col items-end">
+        <SearchProposalInput
+          handleSearchQueryChange={handleSearchQueryChange}
+          searchQuery={searchQuery}
+          handleShowAllProposals={handleShowAllProposals}
+        />
+      </div>
     </div>
+
   );
 };
 
