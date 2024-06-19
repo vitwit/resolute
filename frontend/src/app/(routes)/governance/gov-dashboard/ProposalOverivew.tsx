06ae9cbe975f2cfa4fe6ae1b8957db4058ba95e0
pavania1 <adinapavani@gmail.com>
2024-06-19 12:08:18 +0530

    chore: Governance dashboard

--- frontend/src/app/(routes)/governance/gov-dashboard/ProposalOverivew.tsx
+++ frontend/src/app/(routes)/governance/gov-dashboard/ProposalOverivew.tsx
@@ -53,65 +53,63 @@ const ProposalOverview = ({
     <div className="proposal-view">
       <div className="flex flex-col justify-between h-full">
         <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-10 w-full">
-            <div className="items-start">
-              <button className="secondary-btn" onClick={onClose}>
-                Close
-              </button>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center gap-4 justify-between">
-                <div className="flex space-x-1 items-center">
-                  <p
-                    onClick={navigateToProposal}
-                    className="text-h2 max-w-[400px] truncate cursor-pointer"
-                  >
-                    {proposalTitle}
-                  </p>
-                  <Image
-                    src={REDIRECT_ICON}
-                    width={24}
-                    height={24}
-                    alt="View Proposal"
-                    onClick={navigateToProposal}
-                    className="cursor-pointer"
-                  />
-                </div>
-                {isActive ? (
-                  <div className="active-badge">Active</div>
-                ) : (
-                  <div className="deposit-badge">Deposit</div>
-                )}
+          <div className="items-start">
+            <button className="secondary-btn" onClick={onClose}>
+              Go back
+            </button>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-4 justify-between">
+              <div className="flex space-x-1 items-center">
+                <p
+                  onClick={navigateToProposal}
+                  className="text-h2 max-w-[400px] truncate cursor-pointer"
+                >
+                  {proposalTitle}
+                </p>
+                <Image
+                  src={REDIRECT_ICON}
+                  width={24}
+                  height={24}
+                  alt="View Proposal"
+                  onClick={navigateToProposal}
+                  className="cursor-pointer"
+                />
               </div>
-              <div className="flex gap-6">
-                <div className="flex gap-2 items-center">
-                  <p className="text-small-light ">
-                    {isActive ? 'Voting' : 'Deposit'}
-                  </p>
-                  <p className="text-b1">Ends in {endTime}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <p className="text-small-light ">on</p>
-                  <div className="flex gap-[2px] items-center">
-                    <Image
-                      src={chainLogo}
-                      width={20}
-                      height={20}
-                      alt="Network-logo"
-                    />
-                    <p className="text-b1 capitalize">{chainName}</p>
-                  </div>
+              {isActive ? (
+                <div className="active-badge">Active</div>
+              ) : (
+                <div className="deposit-badge">Deposit</div>
+              )}
+            </div>
+            <div className="flex gap-4">
+              <div className="flex gap-2 items-center">
+                <p className="text-small-light ">
+                  {isActive ? 'Voting' : 'Deposit'}
+                </p>
+                <p className="text-b1">Ends in {endTime}</p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <p className="text-small-light ">on</p>
+                <div className="flex gap-1 items-center">
+                  <Image
+                    src={chainLogo}
+                    width={20}
+                    height={20}
+                    alt="Network-logo"
+                  />
+                  <p className="text-b1 capitalize">{chainName}</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex gap-4 flex-col">
-            <div className="text-white text-base">Proposal Summary</div>
-            <div className="divider-line"></div>
-            <div className="secondary-text">
-              {truncatedDescription || proposalTitle}
-              {isDescriptionTruncated && '...'}
-            </div>
+        </div>
+        <div className="flex gap-4 flex-col">
+          <div className="text-white text-base">Proposal Summary</div>
+          <div className="divider-line"></div>
+          <div className="secondary-text">
+            {truncatedDescription || proposalTitle}
+            {isDescriptionTruncated && '...'}
           </div>
         </div>
 
