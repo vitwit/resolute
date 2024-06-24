669d90ebb466980567e0e9bccd88d1b908cbc1a2
hemanthghs <ghshemanth@gmail.com>
2024-06-24 12:47:31 +0530

    chore: update api for authService

--- frontend/src/store/features/auth/authService.ts
+++ frontend/src/store/features/auth/authService.ts
@@ -1,14 +1,18 @@
 import { AxiosResponse } from 'axios';
 import { axiosGetRequestWrapper } from '@/utils/RequestWrapper';
+import { addChainIDParam } from '@/utils/util';
 
 const accountInfoURL = '/cosmos/auth/v1beta1/accounts/';
 
 const fetchAccountInfo = (
   baseURLs: string[],
-  address: string
+  address: string,
+  chainID: string
   /* eslint-disable @typescript-eslint/no-explicit-any */
 ): Promise<AxiosResponse<any>> => {
-  const endPoint = `${accountInfoURL}${address}`;
+  let endPoint = `${accountInfoURL}${address}`;
+  endPoint = addChainIDParam(endPoint, chainID);
+
   return axiosGetRequestWrapper(baseURLs, endPoint);
 };
 
