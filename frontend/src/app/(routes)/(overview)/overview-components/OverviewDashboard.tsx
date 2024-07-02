58d46e4422fadff5a46325e705fdb4f769328e4c
pavania1 <adinapavani@gmail.com>
2024-07-02 21:31:55 +0530

    chore: dashboard overview

---
+++ frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AssetsTable from './AssetsTable';
+import TokenAllocation from './TokenAllocation';
+import BalanceSummary from './BalanceSummary';
+import GovernanceView from './GovernanceView';
+
+const OverviewDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
+  return (
+    <div className="flex pt-10 gap-10">
+      <div className="flex flex-1">
+        <div className="flex flex-col gap-10">
+          <BalanceSummary chainIDs={chainIDs} />
+          <AssetsTable chainIDs={chainIDs} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-10 h-full">
+        <TokenAllocation />
+        <GovernanceView />
+      </div>
+    </div>
+  );
+};
+
+export default OverviewDashboard;
