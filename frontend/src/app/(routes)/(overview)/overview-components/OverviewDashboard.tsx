73461b78fcf4e2f08219667623a8e4d0b26c4978
hemanthghs <ghshemanth@gmail.com>
2024-07-05 12:05:01 +0530

    chore: fix assets overview

--- frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
+++ frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AssetsTable from './AssetsTable';
 import TokenAllocation from './TokenAllocation';
 import BalanceSummary from './BalanceSummary';
 import GovernanceView from './GovernanceView';
+import { useAppDispatch, useAppSelector } from '@/custom-hooks/StateHooks';
+import { getAllTokensPrice } from '@/store/features/common/commonSlice';
+import { getBalances } from '@/store/features/bank/bankSlice';
+import {
+  getDelegations,
+  getUnbonding,
+} from '@/store/features/staking/stakeSlice';
+import { getAccountInfo } from '@/store/features/auth/authSlice';
+import { getDelegatorTotalRewards } from '@/store/features/distribution/distributionSlice';
 
 const OverviewDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
+  const dispatch = useAppDispatch();
+  const networks = useAppSelector((state) => state.wallet.networks);
+  useEffect(() => {
+    dispatch(getAllTokensPrice());
+
+    chainIDs.forEach((chainID) => {
+      const allChainInfo = networks[chainID];
+      const chainInfo = allChainInfo.network;
+      const address = allChainInfo?.walletInfo?.bech32Address;
+
+      const minimalDenom =
+        allChainInfo.network.config.stakeCurrency.coinMinimalDenom;
+      const basicChainInputs = {
+        baseURL: chainInfo.config.rest,
+        baseURLs: chainInfo.config.restURIs,
+        address,
+        chainID,
+      };
+      dispatch(getBalances(basicChainInputs));
+      dispatch(getDelegations(basicChainInputs));
+      dispatch(getAccountInfo(basicChainInputs));
+      dispatch(
+        getDelegatorTotalRewards({
+          baseURLs: chainInfo.config.restURIs,
+          baseURL: chainInfo.config.rest,
+          address: address,
+          chainID: chainID,
+          denom: minimalDenom,
+        })
+      );
+      dispatch(
+        getUnbonding({
+          baseURLs: chainInfo.config.restURIs,
+          address: address,
+          chainID,
+        })
+      );
+    });
+  }, []);
   return (
     <div>
       <div className="flex pt-10 gap-10">
