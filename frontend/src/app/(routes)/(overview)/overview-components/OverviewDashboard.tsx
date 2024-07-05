f1201aa670ce14b11ca54d076535e9e0e1f222f3
pavania1 <adinapavani@gmail.com>
2024-07-05 10:56:13 +0530

    chore: skeleton

--- frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
+++ frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
@@ -3,20 +3,24 @@ import AssetsTable from './AssetsTable';
 import TokenAllocation from './TokenAllocation';
 import BalanceSummary from './BalanceSummary';
 import GovernanceView from './GovernanceView';
+import DashboardLoading from './DashboardLoading';
 
 const OverviewDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
   return (
-    <div className="flex pt-10 gap-10">
-      <div className="flex flex-1">
-        <div className="flex flex-col gap-10">
-          <BalanceSummary chainIDs={chainIDs} />
-          <AssetsTable chainIDs={chainIDs} />
+    <div>
+      <div className="flex pt-10 gap-10">
+        <div className="flex flex-1">
+          <div className="flex flex-col gap-10">
+            <BalanceSummary chainIDs={chainIDs} />
+            <AssetsTable chainIDs={chainIDs} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-10 h-[calc(100vh-104px)]">
+          <TokenAllocation />
+          <GovernanceView chainIDs={chainIDs} />
         </div>
       </div>
-      <div className="flex flex-col gap-10 h-[calc(100vh-104px)]">
-        <TokenAllocation />
-        <GovernanceView chainIDs={chainIDs} />
-      </div>
+      {/* <DashboardLoading /> */}
     </div>
   );
 };
