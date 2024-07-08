73dacd303690acbe11249776667e0f45c5466cdc
pavania1 <adinapavani@gmail.com>
2024-07-08 11:34:52 +0530

    chore: spacing for governance view

--- frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
+++ frontend/src/app/(routes)/(overview)/overview-components/OverviewDashboard.tsx
@@ -63,7 +63,7 @@ const OverviewDashboard = ({ chainIDs }: { chainIDs: string[] }) => {
             <AssetsTable chainIDs={chainIDs} />
           </div>
         </div>
-        <div className="flex flex-col gap-10 h-[calc(100vh-104px)]">
+        <div className="flex flex-col gap-10 h-[calc(100vh-104px)] pb-3">
           <TokenAllocation />
           <GovernanceView chainIDs={chainIDs} />
         </div>
