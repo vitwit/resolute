5e4f488a80b2819abb95402ba310d8055b5a8948
pavania1 <adinapavani@gmail.com>
2024-07-09 15:53:36 +0530

    chore: illustration for withconnection

--- frontend/src/components/illustrations/withConnectionIllustration.tsx
+++ frontend/src/components/illustrations/withConnectionIllustration.tsx
@@ -2,30 +2,18 @@ import Image from 'next/image';
 
 const WithConnectionIllustration = ({ message }: { message: string }) => {
   return (
-    <div className="flex flex-col justify-center  gap-10 w-full px-10 mt-20">
-      {/* <div className="flex flex-col items-start space-y-1">
-        <div className="text-h1">Staking</div>
-        <div className="text-b1-light">
-          Connect your wallet now to access all the modules on resolute{' '}
-        </div>
-        <div className="divider-line"></div>
-      </div> */}
-
-      <div className="flex flex-col justify-center items-center gap-10">
-        <div className="flex flex-col gap-6">
-          <Image
-            src="/illustrate.png"
-            width={232}
-            height={200}
-            alt="Illustration"
-            draggable={false}
-          />
-          <div className="flex flex-col justify-center items-center w-full">
-            <p className="empty-screen-title"> {message} </p>
-            {/* <p className="secondary-text">
-              Connect your wallet to access your account on Resolute
-            </p> */}
-          </div>
+    <div className="flex flex-col justify-center gap-10 w-full px-10">
+      <div className="flex flex-col justify-center items-center gap-10 relative">
+        <Image
+          src="/illustrate.png"
+          width={180}
+          height={180}
+          alt="Illustration"
+          draggable={false}
+          className="opacity-20"
+        />
+        <div className="absolute flex flex-col justify-center items-center w-full h-full top-0 left-0">
+          <p className="empty-screen-title text-center">{message}</p>
         </div>
       </div>
     </div>
