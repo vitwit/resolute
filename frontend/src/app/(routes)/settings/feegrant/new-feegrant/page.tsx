e5c5ab12b66c1e7e97d2a7733245352649f319bf
hemanthghs <ghshemanth@gmail.com>
2024-08-01 11:44:15 +0530

    wip

--- frontend/src/app/(routes)/settings/feegrant/new-feegrant/page.tsx
+++ frontend/src/app/(routes)/settings/feegrant/new-feegrant/page.tsx
@@ -2,9 +2,23 @@
 
 import React from 'react';
 import '../../settings.css';
+import Link from 'next/link';
+import PageHeader from '@/components/common/PageHeader';
 
 const page = () => {
-  return <div className="">New Feegrant</div>;
+  return (
+    <div className="mt-10">
+      <div>
+        <Link
+          href="/settings/feegrant"
+          className="text-btn h-8 flex items-center w-fit"
+        >
+          <span>Back</span>
+        </Link>
+        <PageHeader title="New Feegrant" description="New Feegrant" />
+      </div>
+    </div>
+  );
 };
 
 export default page;
