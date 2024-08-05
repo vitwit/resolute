46572a771994d8f8c6e6b33554d9c06a44c8fc3e
hemanthghs <ghshemanth@gmail.com>
2024-08-05 11:41:12 +0530

    add link to new feegrant

--- frontend/src/app/(routes)/settings/feegrant/[network]/page.tsx
+++ frontend/src/app/(routes)/settings/feegrant/[network]/page.tsx
@@ -3,9 +3,14 @@
 import React from 'react';
 import '../../settings.css';
 import SettingsLayout from '../../SettingsLayout';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
-  const createNewFeegrant = () => {};
+  const router = useRouter();
+  const createNewFeegrant = () => {
+    router.push('/settings/feegrant/new-feegrant');
+  };
+
   return (
     <SettingsLayout
       action={createNewFeegrant}
