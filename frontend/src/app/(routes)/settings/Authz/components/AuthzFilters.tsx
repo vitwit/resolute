0b9454265e7b61b4d38de65823c8cd4278014ebc
pavania1 <adinapavani@gmail.com>
2024-08-07 15:10:34 +0530

    chore: AuthzDashboard both grantbyme and granttome

---
+++ frontend/src/app/(routes)/settings/Authz/components/AuthzFilters.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import GrantedByMe from './GrantedByMe';
+import GrantedToMe from './GrantedToMe';
+
+const FeegrantFilters = () => {
+  const [filter, setFilter] = useState('byMe');
+
+  const handleFiltersChange = (newFilter: string) => {
+    setFilter(newFilter);
+  };
+  return (
+    <div className="  w-full px-6">
+      <div className="flex py-2 gap-2 w-full">
+        <button
+          onClick={() => handleFiltersChange('toMe')}
+          className={`selected-filter text-[14px] ${
+            filter === 'toMe'
+              ? 'bg-[#ffffff14] border-transparent'
+              : 'border-[#ffffff26]'
+          }`}
+        >
+          Granted to me
+        </button>
+        <button
+          onClick={() => handleFiltersChange('byMe')}
+          className={`selected-filter text-[14px] ${
+            filter === 'byMe'
+              ? 'bg-[#ffffff14] border-transparent'
+              : 'border-[#ffffff26]'
+          }`}
+        >
+          Granted by me
+        </button>
+      </div>
+      {filter === 'byMe' ? (
+        <div className="">
+          <GrantedByMe />
+        </div>
+      ) : null}
+      {filter === 'toMe' ? (
+        <div className="">
+          <GrantedToMe />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default FeegrantFilters;
---
+++ frontend/src/app/(routes)/settings/Authz/components/GrantedToMe.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Copy from '@/components/common/Copy';
+import DialogAuthzDetails from './DialogAuthzDetails';
+import { TICK_ICON } from '@/constants/image-names';
+
+type Grant = {
+  address: string;
+  permissions: string[];
+};
+
+const GrantedToMe = () => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(
+    null
+  );
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const handleViewDetails = () => {
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  const grants: Grant[] = [
+    {
+      address: 'pasg1y0hvu8ts6m87yltguyufwf',
+      permissions: ['send', 'Delegate', 'vote', 'send'],
+    },
+    {
+      address: 'pasg1xy2a8ts6m87yltguyufwf',
+      permissions: ['send', 'Delegate', 'vote', 'send'],
+    },
+    {
+      address: 'pasg1y0hvu8ts6m87yltguyufwf',
+      permissions: [
+        'send',
+        'Delegate',
+        'vote',
+        'send',
+        'send',
+        'Delegate',
+        'vote',
+        'send',
+      ],
+    },
+    {
+      address: 'pasg1xy2a8ts6m87yltguyufwf',
+      permissions: ['send', 'Delegate', 'vote', 'send'],
+    },
+  ];
+
+  const handleSelectCard = (index: number) => {
+    setSelectedCardIndex(selectedCardIndex === index ? null : index);
+  };
+
+  return (
+    <div className="space-y-6 pt-6">
+      {grants.map((grant, index) => (
+        <div
+          className={`grants-card justify-between w-full  gap-16 ${
+            selectedCardIndex === index ? 'selected-grants-card' : ''
+          }`}
+          key={index}
+        >
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center">
+              <p className="text-b1-light">Address</p>
+              {selectedCardIndex === index && (
+                <div className="flex space-x-0">
+                <Image src={TICK_ICON} width={16} height={16} alt="used-icon" />
+                <span className="text-[#2BA472]">Currently Using</span>
+              </div>
+              )}
+            </div>
+            <div className="flex gap-2 items-center h-8">
+              <p className="truncate text-b1">{grant.address}</p>
+              <Copy content={grant.address} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-b1-light">Permissions</p>
+            <div className="grid grid-cols-4 gap-2">
+              {grant.permissions.map((permission, idx) => (
+                <div
+                  className="permission-card flex gap-2 items-center"
+                  key={idx}
+                >
+                  <p className="text-b1">{permission}</p>
+                  <Image
+                    src="/akash.png"
+                    width={20}
+                    height={20}
+                    alt="network-logo"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex gap-6 items-end">
+            <button
+              className={
+                selectedCardIndex === index ? 'cancel-btn' : 'primary-btn'
+              }
+              onClick={() => handleSelectCard(index)}
+            >
+              {selectedCardIndex === index ? 'Cancel' : 'Using'}
+            </button>
+            <div className="secondary-btn" onClick={handleViewDetails}>
+              View Details
+            </div>
+          </div>
+        </div>
+      ))}
+      <DialogAuthzDetails open={dialogOpen} onClose={handleCloseDialog} />
+    </div>
+  );
+};
+
+export default GrantedToMe;
