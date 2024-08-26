1594d9188b3b3784ef31896f9e64432841345ef1
pavania1 <adinapavani@gmail.com>
2024-08-26 11:10:47 +0530

    chore: tabs

--- frontend/src/app/(routes)/cosmwasm/components/deploy-contract/DeployContract.tsx
+++ frontend/src/app/(routes)/cosmwasm/components/deploy-contract/DeployContract.tsx
@@ -1,11 +1,13 @@
 import PageHeader from '@/components/common/PageHeader';
 import { DEPLOY_CONTRACT_DESCRIPTION } from '@/utils/constants';
 import React from 'react';
+import DeployContractTabs from './DeployContractTabs';
 
 const DeployContract = () => {
   return (
     <div className="py-10 h-full space-y-10 min-h-[100vh]">
       <PageHeader title="Deploy" description={DEPLOY_CONTRACT_DESCRIPTION} />
+      <DeployContractTabs />
     </div>
   );
 };
---
+++ frontend/src/app/(routes)/cosmwasm/components/deploy-contract/DeployContractTabs.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const DeployContractTabs = () => {
+  const tabs = ['Use Existing code ID', 'Upload  WASM file'];
+  const [selectedTab, setSelectedTab] = useState('Use Existing code ID');
+  return (
+    <div className="space-y-10 pb-10">
+      <div className="tabs flex gap-6">
+        {tabs.map((tab) => (
+          <div key={tab} className="w-full justify-between gap-10">
+            <div
+              className={`selected-filters
+                  ${
+                    selectedTab.toLowerCase() === tab.toLowerCase()
+                      ? 'bg-[#ffffff14] border-transparent'
+                      : 'border-[#ffffff26]'
+                  }
+                `}
+              onClick={() => {
+                setSelectedTab(tab);
+              }}
+            >
+              {tab}
+            </div>
+            <div
+              className={
+                selectedTab.toLowerCase() === tab.toLowerCase()
+                  ? 'bg-[#ffffff14] border-transparent'
+                  : 'border-[#ffffff26]'
+              }
+            ></div>
+          </div>
+        ))}
+      </div>
+      {/* <div className="">
+        {selectedTab === 'Query Contract' ? (
+          <QueryContract
+            address={selectedContractAddress}
+            baseURLs={restURLs}
+            chainID={chainID}
+          />
+        ) : (
+          <ExecuteContract
+            address={selectedContractAddress}
+            baseURLs={restURLs}
+            chainID={chainID}
+            rpcURLs={rpcURLs}
+            walletAddress={walletAddress}
+            chainName={chainName}
+          />
+        )}
+      </div> */}
+    </div>
+  );
+};
+
+export default DeployContractTabs;
