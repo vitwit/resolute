dd45bbdcc209f88defae1236511efc813005e2c1
pavania1 <adinapavani@gmail.com>
2024-08-26 11:10:25 +0530

    chore: header

--- frontend/src/app/(routes)/cosmwasm/components/deploy-contract/DeployContract.tsx
+++ frontend/src/app/(routes)/cosmwasm/components/deploy-contract/DeployContract.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import PageHeader from '@/components/common/PageHeader';
+import { DEPLOY_CONTRACT_DESCRIPTION } from '@/utils/constants';
+import React from 'react';
 
 const DeployContract = () => {
   return (
-    <div>DeployContract</div>
-  )
-}
+    <div className="py-10 h-full space-y-10 min-h-[100vh]">
+      <PageHeader title="Deploy" description={DEPLOY_CONTRACT_DESCRIPTION} />
+    </div>
+  );
+};
 
-export default DeployContract
+export default DeployContract;
