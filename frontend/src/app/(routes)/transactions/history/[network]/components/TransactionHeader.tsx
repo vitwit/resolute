2ad85f90da7f34d8dd5509b827ff0370513f6079
charymalloju <chary@vitwit.com>
2024-09-05 12:53:58 +0530

    fix log error log

--- frontend/src/app/(routes)/transactions/history/[network]/components/TransactionHeader.tsx
+++ frontend/src/app/(routes)/transactions/history/[network]/components/TransactionHeader.tsx
@@ -15,6 +15,7 @@ interface TransactionHeaderProps {
   goBackUrl: string;
   isSearchPage?: boolean;
   mintscanURL: string;
+  rawLog?: string;
 }
 
 const TransactionHeader: React.FC<TransactionHeaderProps> = ({
@@ -25,6 +26,7 @@ const TransactionHeader: React.FC<TransactionHeaderProps> = ({
   goBackUrl,
   isSearchPage,
   mintscanURL,
+  rawLog
 }) => {
   const isSuccess = status === 'success';
   const textColorClass = isSuccess ? 'text-[#2BA472]' : 'text-[#FA5E42]';
@@ -37,56 +39,67 @@ const TransactionHeader: React.FC<TransactionHeaderProps> = ({
             Go back
           </Link>
         )}
-        <div className="flex justify-between items-end gap-6">
-          <div className="flex gap-1 flex-col flex-1">
-            <div className="flex gap-1 items-center">
-              <Image
-                src={isSuccess ? '/success-icon.svg' : '/failed-icon.svg'}
-                width={32}
-                height={32}
-                alt={isSuccess ? 'success-icon' : 'failed-icon'}
-              />
-              <div className={`text-[20px] font-bold ${textColorClass}`}>
-                {isSuccess ? 'Transaction Successful' : 'Transaction Failed'}
-              </div>
-            </div>
-            {!isSuccess && <div className="text-b1-light"></div>}
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-1">
-                <div className="text-b1-light">{hash}</div>
-                <Copy content={hash} />
-              </div>
-              <div className="divider-line"></div>
+        <div>
+          <div className="flex gap-1 items-center">
+            <Image
+              src={isSuccess ? '/success-icon.svg' : '/failed-icon.svg'}
+              width={32}
+              height={32}
+              alt={isSuccess ? 'success-icon' : 'failed-icon'}
+            />
+            <div className={`text-[20px] font-bold ${textColorClass}`}>
+              {isSuccess ? 'Transaction Successful' : 'Transaction Failed'}
             </div>
           </div>
-          <div className="flex items-center gap-6">
-            {isSearchPage ? null : (
-              <CustomButton
-                btnText={isSuccess ? 'Repeat Transaction' : 'Retry Transaction'}
-                btnDisabled={disableAction}
-                btnOnClick={onRepeatTxn}
-                btnStyles={`${disableAction ? 'opacity-50 cursor-not-allowed' : ''}`}
-              />
-            )}
-            {mintscanURL ? (
-              <Link
-                className="flex items-end gap-1"
-                href={mintscanURL}
-                target="_blank"
-              >
-                <div className="secondary-btn">View on Mintscan</div>
-                <Image
-                  src={REDIRECT_ICON}
-                  width={18}
-                  height={18}
-                  alt="View Proposal"
-                  className="cursor-pointer"
-                  draggable={false}
+          {!isSuccess && <div className="text-b1-light"></div>}
+          {
+            !isSuccess && <div className='w-full'>
+              <div className={`text-[15px] text-opacity-75 ${textColorClass}`}>
+                {isSuccess ? null : rawLog}
+              </div>
+              <br/>
+            </div> || null
+          }
+          <div className="flex justify-between items-end gap-6">
+            <div className="flex gap-1 flex-col flex-1">
+              <div className="flex flex-col gap-2">
+                <div className="flex gap-1">
+                  <div className="text-b1-light">{hash}</div>
+                  <Copy content={hash} />
+                </div>
+                <div className="divider-line"></div>
+              </div>
+            </div>
+            <div className="flex items-center gap-6">
+              {isSearchPage ? null : (
+                <CustomButton
+                  btnText={isSuccess ? 'Repeat Transaction' : 'Retry Transaction'}
+                  btnDisabled={disableAction}
+                  btnOnClick={onRepeatTxn}
+                  btnStyles={`${disableAction ? 'opacity-50 cursor-not-allowed' : ''}`}
                 />
-              </Link>
-            ) : null}
+              )}
+              {mintscanURL ? (
+                <Link
+                  className="flex items-end gap-1"
+                  href={mintscanURL}
+                  target="_blank"
+                >
+                  <div className="secondary-btn">View on Mintscan</div>
+                  <Image
+                    src={REDIRECT_ICON}
+                    width={18}
+                    height={18}
+                    alt="View Proposal"
+                    className="cursor-pointer"
+                    draggable={false}
+                  />
+                </Link>
+              ) : null}
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   );
