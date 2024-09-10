095eba5faac9a9474ae9adb2fac491f6b1cb4ece
hemanthghs <ghshemanth@gmail.com>
2024-09-11 00:43:11 +0530

    chore: update loaders ui

--- frontend/src/app/(routes)/multisig/components/loaders/MultisigInfoLoading.tsx
+++ frontend/src/app/(routes)/multisig/components/loaders/MultisigInfoLoading.tsx
@@ -5,7 +5,7 @@ const MultisigInfoLoading = () => {
     <div>
       <div className="flex flex-col gap-6 w-full">
         <span className="secondary-btn">Back to List</span>
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between items-center w-full">
           <div className="space-y-2 w-full">
             <div className="flex gap-2 items-center">
               <p className="w-10 h-10 animate-pulse bg-[#252525] rounded-full"></p>
@@ -28,14 +28,18 @@ const MultisigInfoLoading = () => {
       </div>
 
       <div className="mt-10">
-        <div className="flex justify-between w-full">
-          <div className="flex gap-10">
-            <p className="">Transactions</p>
-            <p className="">Members</p>
+        <div className='flex'>
+          <div className='flex-1 space-y-4'>
+            <div className="flex justify-between w-full">
+              <div className="flex gap-10">
+                <p className="">Transactions</p>
+                <p className="">Members</p>
+              </div>
+            </div>
+            <div className="divider-line w-full"></div>
           </div>
           <button className="w-[146px] !h-8 animate-pulse bg-[#252525] rounded-full"></button>
         </div>
-        <div className="divider-line w-full"></div>
         <div className="flex gap-4 mt-6">
           <p className="w-[120px] h-8 animate-pulse bg-[#252525] rounded" />
           <p className="w-[120px] h-8 animate-pulse bg-[#252525] rounded" />
